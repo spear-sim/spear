989a504303fdd2b5105b384c44c0781cb2546380
Mike Roberts <mike.l.roberts@intel.com>
2021-09-14 12:28:28 -0700

    fixed code formatting

--- code/unreal_plugins/RobotSim/Source/UrdfBot/MeshGeneration/StaticMeshGenerator.cpp
+++ code/unreal_plugins/RobotSim/Source/UrdfBot/MeshGeneration/StaticMeshGenerator.cpp
@@ -1175,14 +1175,14 @@ void StaticMeshGenerator::SplitPartitioner(UModel* Model,
         {
         case SP_Coplanar:
             // May occasionally happen.
-            //				UE_LOG(LogBSPOps, Log,  TEXT("FilterBound:
-            //Got inficoplanar") );
+            //				UE_LOG(LogBSPOps, Log,
+            //TEXT("FilterBound: Got inficoplanar") );
             break;
 
         case SP_Front:
             // Shouldn't happen if hull is correct.
-            //				UE_LOG(LogBSPOps, Log,  TEXT("FilterBound: Got
-            //infifront")
+            //				UE_LOG(LogBSPOps, Log,  TEXT("FilterBound:
+            //Got infifront")
             //);
             return;
 
@@ -1248,8 +1248,8 @@ void StaticMeshGenerator::FilterBound(UModel* Model,
         switch (Poly->SplitWithPlane(Base, Normal, FrontPoly, BackPoly, 0))
         {
         case SP_Coplanar:
-            //				UE_LOG(LogBSPOps, Log,  TEXT("FilterBound: Got
-            //coplanar")
+            //				UE_LOG(LogBSPOps, Log,  TEXT("FilterBound:
+            //Got coplanar")
             //);
             FrontList[nFront++] = Poly;
             BackList[nBack++] = Poly;
