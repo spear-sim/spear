52e671d5e3f79c4957dac235ce91d50aa5d7372a
RachithP <rachithprakash@gmail.com>
2021-09-27 12:37:40 -0700

    add new line eof

--- utils/apply_clang_style.cmd
+++ utils/apply_clang_style.cmd
@@ -1,4 +1,5 @@
 @echo off
 
 REM //---------- apply clang-format to all required .h and .cpp files ----------
-FOR /F "tokens=*" %%G IN ('dir /b /s ..\code\*.h ..\code\*.cpp ^| find /v /i "thirdparty"') DO (clang-format.exe -i -style=file --verbose %%G)
+FOR /F "tokens=*" %%G IN ('dir /b /s ..\code\*.h ..\code\*.cpp ^| find /v /i "thirdparty"') DO (clang-format.exe -i -style=file --verbose %%G
+)
