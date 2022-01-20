452925226424a5634a5c04c37ed8898e1659f82e
RachithP <rachithprakash@gmail.com>
2022-01-20 11:51:56 -0800

    remove UnrealRL, python_client, thirdparty directories

--- code/python_client/unrealai/__init__.py
+++
@@ -1,5 +0,0 @@
-import unrealai.unrealrl
-
-from unrealai.unrealrl.envs import UnrealEnv
-
-from unrealai.unrealrl.wrappers.gym_wrapper import UnrealToGymWrapper
--- code/python_client/unrealai/constants.py
+++
@@ -1,3 +0,0 @@
-import os
-
-PACKAGE_ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
