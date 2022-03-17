5ba88c896b83b5761133cb4bdc0993ca5f08f1a4
rachithp <rachithprakash@gmail.com>
2022-03-17 09:51:45 -0700

    update AgentController.h

--- code/unreal_plugins/SimulationController/Source/SimulationController/Public/AgentController.h
+++ code/unreal_plugins/SimulationController/Source/SimulationController/Public/AgentController.h
@@ -1,5 +1,6 @@
 #pragma once
 
+
 class SIMULATIONCONTROLLER_API AgentController
 {
 public:
@@ -7,41 +8,16 @@ public:
     ~AgentController() = default;
 
     /**
-     * @brief 
-     * 
-     */
-    virtual void Init();
-
-    /**
-     * @brief 
-     * 
-     */
-    virtual void Terminate();
-
-    /**
      * Call this to prep agent as per the values received from @param Action
-     * The actions has to be performed during consequent tick call.
+     * The actions will be performed during consequent tick call.
      * @todo: should we expose this function to blueprint?
      */
-    virtual void SetAction();
+    virtual void applyAction(const std::vector<float>& Action);
 
     /**
-     * Call this to record observation for this agent.
+     * This is to record observations for this agent.
      * This function has to be called post tick call to get new observations.
      * @todo: should we expose this function to blueprint?
      */
-    virtual void
-    GetObservation();
-
-    /**
-     * Use this to set agent's initial state i.e state at the beginning of an
-     * episode. At the beginning of each episode, this function will be called.
-     */
-    virtual void BeginEpisode();
-
-    /**
-     * Use this to check if agent/actor has met certain conditions that
-     * classifies it as experiment ready. If yes, set return true.
-     */
-    virtual bool IsAgentReady();
+    virtual std::vector<uint8_t>& getObservation();
 };
