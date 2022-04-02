18184c4c6d50f6ac8818aabdbef13975e02f9eb0
rachithp <rachithprakash@gmail.com>
2022-04-01 17:01:49 -0700

    add CoreUtils plugin

---
+++ code/unreal_plugins/CoreUtils/Source/Config.cpp
@@ -0,0 +1,41 @@
+#include "Config.h"
+
+#include "IgnoreCompilerWarnings.h"
+ENABLE_IGNORE_COMPILER_WARNINGS
+#include <yaml-cpp/yaml.h>
+DISABLE_IGNORE_COMPILER_WARNINGS
+
+#include <Misc/CommandLine.h>
+#include <Misc/Parse.h>
+#include <Misc/Paths.h>
+
+#include "Assert.h"
+
+// define static member variable
+YAML::Node Config::config_node_;
+
+void Config::initialize()
+{
+    // Path to the file that contains config values.
+    FString config_file;
+
+    // Load config_node_ from config file provided via command line.
+    if (FParse::Value(FCommandLine::Get(), TEXT("configfile="), config_file)) {
+        // Read config file contents into a YAML::Node.
+        config_node_ = YAML::LoadFile(TCHAR_TO_UTF8(*config_file));
+    }
+    // Load config_node_ from project dir.
+    else if (FPaths::FileExists(FPaths::ConvertRelativePathToFull(FPaths::ProjectDir().Append("Temp/config.yaml")))) {
+        // Read config file contents into a YAML::Node.
+        config_file = FPaths::ConvertRelativePathToFull(FPaths::ProjectDir().Append("Temp/config.yaml"));
+        config_node_ = YAML::LoadFile(TCHAR_TO_UTF8(*config_file));
+    }
+    else {
+        ASSERT(false, "Did not receive a config file location from python and could not find config.yaml in project's 'Temp' directory. So, tried to launch project without a config file. Currently we do not support this.");
+    }
+}
+
+void Config::terminate()
+{
+    config_node_.reset();
+}
