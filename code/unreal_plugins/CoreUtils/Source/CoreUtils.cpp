ea2301c18a8c20d4789fdb7cc9669f9c5a4ee032
rachithp <rachithprakash@gmail.com>
2022-04-01 17:08:49 -0700

    add CoreUtils plugin missing fiels

---
+++ code/unreal_plugins/CoreUtils/Source/CoreUtils.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CoreUtils.h"
+
+void CoreUtils::StartupModule() {}
+
+void CoreUtils::ShutdownModule() {}
+
+IMPLEMENT_MODULE(CoreUtils, CoreUtils)
