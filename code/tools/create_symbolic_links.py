08ea6261107189153634b1e8bfd8c18e042e56a1
Mike Roberts <mike.l.roberts@intel.com>
2022-04-14 02:24:57 -0700

    fixes to make the getting started tutorial work correctly, cleanup, improved build.cs files

--- code/tools/create_symbolic_links.py
+++ code/tools/create_symbolic_links.py
@@ -1,15 +1,36 @@
 import json
 import os
 
-SCRIPT_DIR_PATH = os.path.dirname(os.path.abspath(__file__))
+SCRIPT_DIR_PATH      = os.path.dirname(os.path.abspath(__file__))
 UNREAL_PROJECTS_PATH = os.path.join(SCRIPT_DIR_PATH, "..", "unreal_projects")
-UNREAL_PLUGINS_PATH = os.path.join(SCRIPT_DIR_PATH, "..", "unreal_plugins")
+UNREAL_PLUGINS_PATH  = os.path.join(SCRIPT_DIR_PATH, "..", "unreal_plugins")
+THIRD_PARTY_PATH     = os.path.join(SCRIPT_DIR_PATH, "..", "third_party")
 
 def create_symbolic_links():
 
     # get list of the projects and plugins we maintain
     our_projects = os.listdir(UNREAL_PROJECTS_PATH)
     our_plugins = os.listdir(UNREAL_PLUGINS_PATH)
+
+
+    # for each plugin
+    for plugin in our_plugins:
+
+        # if the plugin is a valid plugin (i.e., plugin dir has a uplugin file)
+        uplugin = os.path.join(UNREAL_PLUGINS_PATH, plugin, plugin+'.uplugin')
+        if os.path.exists(uplugin):
+
+            print(f"Found plugin: {plugin}")
+
+            # create a symlink to code/third_party
+            symlink_third_party_path = os.path.join(UNREAL_PLUGINS_PATH, plugin, "ThirdParty")
+            if not os.path.exists(symlink_third_party_path):
+                print(f"    Creating symlink: {symlink_third_party_path} -> {THIRD_PARTY_PATH}")
+                os.symlink(THIRD_PARTY_PATH, symlink_third_party_path)
+            else:
+                print(f"    {symlink_third_party_path} already exists, so we do not create a symlink...")
+    print()
+
 
     # for each project
     for project in our_projects:
@@ -18,10 +39,20 @@ def create_symbolic_links():
         uproject = os.path.join(UNREAL_PROJECTS_PATH, project, project+'.uproject')
         if os.path.exists(uproject):
 
+            print(f"Found project: {project}")
+
+            # create a symlink to code/third_party
+            symlink_third_party_path = os.path.join(UNREAL_PROJECTS_PATH, project, "ThirdParty")
+            if not os.path.exists(symlink_third_party_path):
+                print(f"    Creating symlink: {symlink_third_party_path} -> {THIRD_PARTY_PATH}")
+                os.symlink(THIRD_PARTY_PATH, symlink_third_party_path)
+            else:
+                print(f"    {symlink_third_party_path} already exists, so we do not create a symlink...")
+
             # get list of plugins from the uproject file
             with open(os.path.join(UNREAL_PROJECTS_PATH, project, project+'.uproject')) as f:
                 project_plugins = [ p["Name"] for p in json.load(f)["Plugins"] ]
-            print(f"{project} plugin dependencies: {project_plugins}")
+            print(f"    Plugin dependencies: {project_plugins}")
 
             # create a Plugins dir in the project dir
             project_plugins_dir = os.path.join(UNREAL_PROJECTS_PATH, project, "Plugins")
@@ -34,14 +65,17 @@ def create_symbolic_links():
                     our_plugin_path = os.path.abspath(os.path.join(UNREAL_PLUGINS_PATH, project_plugin))
                     symlink_plugin_path = os.path.abspath(os.path.join(project_plugins_dir, project_plugin))
                     if not os.path.exists(symlink_plugin_path):
-                        print(f"    Creating symlink: {symlink_plugin_path} -> {our_plugin_path}")
+                        print(f"        Creating symlink: {symlink_plugin_path} -> {our_plugin_path}")
                         os.symlink(our_plugin_path, symlink_plugin_path)
                     else:
-                        print(f"    {symlink_plugin_path} already exists, so we do not create a symlink...")
+                        print(f"        {symlink_plugin_path} already exists, so we do not create a symlink...")
                 else:
-                    print(f"    {project} depends on {project_plugin}, but this plugin is not in {UNREAL_PLUGINS_PATH}, so we do not create a symlink...")
+                    print(f"        {project} depends on {project_plugin}, but this plugin is not in {UNREAL_PLUGINS_PATH}, so we do not create a symlink...")
+    print()
+
 
     print(f"Done.")
+
 
 if __name__ == "__main__":    
     create_symbolic_links()    
---
+++ code/unreal_plugins/SimulationController/Source/SimulationController.Build.cs
@@ -0,0 +1,41 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System;
+using System.Diagnostics;
+using System.IO;
+
+public class SimulationController : ModuleRules
+{
+    public SimulationController(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+        bEnableExceptions = true;
+
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "CoreUtils", "Engine", "InputCore", "RenderCore", "RHI" });
+        PrivateDependencyModuleNames.AddRange(new string[] { });
+
+        //
+        // asio
+        //
+
+        PublicDefinitions.Add("ASIO_NO_TYPEID");
+        PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "..", "ThirdParty", "asio", "asio", "include"));
+
+        //
+        // rpclib
+        //
+
+        PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "..", "ThirdParty", "rpclib", "include"));
+
+        if (Target.Platform == UnrealTargetPlatform.Win64) {
+            PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "..", "ThirdParty", "rpclib", "build", "rpc.lib"));
+        } else if (Target.Platform == UnrealTargetPlatform.Mac) {
+            PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "..", "ThirdParty", "rpclib", "build", "librpc.a"));
+        } else if (Target.Platform == UnrealTargetPlatform.Linux) {
+            PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "..", "ThirdParty", "rpclib", "build", "librpc.a"));
+        } else {
+            throw new Exception("Unexpected: Target.Platform == " + Target.Platform);
+        }
+    }
+}
