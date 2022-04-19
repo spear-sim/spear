75ebc99c7e335c6d470ac5c9dfb0577c2db66bfe
Mike Roberts <mike.l.roberts@intel.com>
2022-04-18 21:32:32 -0700

    fixes to InteriorEnvironment example project

--- code/unreal_plugins/SimulationController/Source/SimulationController.Build.cs
+++ code/unreal_plugins/SimulationController/Source/SimulationController.Build.cs
@@ -13,7 +13,7 @@ public class SimulationController : ModuleRules
         bEnableExceptions = true;
 
         PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "CoreUtils", "Engine", "InputCore", "RenderCore", "RHI" });
-        PrivateDependencyModuleNames.AddRange(new string[] { });
+        PrivateDependencyModuleNames.AddRange(new string[] {});
 
         //
         // asio
