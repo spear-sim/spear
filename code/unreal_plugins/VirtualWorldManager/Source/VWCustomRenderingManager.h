a48602ad645f2f7f4a68404f55f09f497a5a39af
rwang15 <xichen@qunhemail.com>
2022-07-04 20:30:55 +0800

    format rendering manager

--- code/unreal_plugins/VirtualWorldManager/Source/VWCustomRenderingManager.h
+++ code/unreal_plugins/VirtualWorldManager/Source/VWCustomRenderingManager.h
@@ -4,24 +4,14 @@
 
 #include <vector>
 
-#include "VWCustomRenderingManager.generated.h"
-
-UCLASS()
-class VIRTUALWORLDMANAGER_API UVWCustomRenderingManager : public UObject
+class VIRTUALWORLDMANAGER_API UVWCustomRenderingManager
 {
 public:
-    GENERATED_BODY()
-    UVWCustomRenderingManager();
 	/**
      * @brief achieve lambertian rendering by disable metallic, roughness and specular parts in asset material
      *
      * @param actors - actors in the world that like to change rendering mode
      * @param enable - enable lambert rendering
      */
-	void setLambertianRendering(std::vector<AActor*>& actors, bool enable);
-	
-    UMaterialInterface* pp_material_diffuse_ = nullptr;
-    UMaterialInterface* pp_material_cel_shader_ = nullptr;
-    UMaterialInterface* pp_material_painter_ = nullptr;
-
+	static void setLambertianRendering(std::vector<AActor*>& actors, bool enable);
 };
