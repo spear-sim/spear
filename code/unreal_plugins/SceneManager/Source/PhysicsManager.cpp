a1f2d96f590dce85b35fbf3e0ba02de755888c4b
rwang15 <xichen@qunhemail.com>
2022-07-21 19:33:54 +0800

    functionality - change physical material

--- code/unreal_plugins/SceneManager/Source/PhysicsManager.cpp
+++ code/unreal_plugins/SceneManager/Source/PhysicsManager.cpp
@@ -9,7 +9,7 @@ void PhysicsManager::setPhysicalMaterial(const std::vector<AActor*>& actors, int
     //find physical material uasset
     char char_array[100];
     snprintf(char_array, sizeof(char_array), "/Game/Scene/PhyMaterials/PM_%d.PM_%d", physical_material_id,physical_material_id);
-    UPhysicalMaterial* override_physical_material = LoadObject<UPhysicalMaterial>(nullptr, *FString(UTF8_TO_TCHAR(char_array)));
+    UPhysicalMaterial* override_physical_material = LoadObject<UPhysicalMaterial>(nullptr, UTF8_TO_TCHAR(char_array));
 
     // check if physical material is valid
     ASSERT(override_physical_material);
