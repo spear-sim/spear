7c82a8960331ab0fde4950afc2f654e75d9fc2aa
rwang15 <36141951+rwang15@users.noreply.github.com>
2022-08-15 21:27:59 +0800

    scenemanager - set physcial material (#87)
    * move folder for example project
    
    * add physical property manager and example
    
    * createPhysicalMaterial
    
    * terminate - only clear pointer, do not destroy UObject
    
    * fix standalone issue
    
    * rollback setActorPhysicalMaterials implementation
    
    * create new physical material
    
    * code format
    
    Co-authored-by: rwang15 <xichen@qunhemail.com>

--- code/experiments/scene_manager_example/SceneManagerExample/Source/SceneManagerExample.Build.cs
+++ code/experiments/scene_manager_example/SceneManagerExample/Source/SceneManagerExample.Build.cs
@@ -8,7 +8,7 @@ public class SceneManagerExample : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" ,"SceneManager"});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
--- code/experiments/scene_manager_example/SceneManagerExample/Source/SceneManagerExample.h
+++ code/experiments/scene_manager_example/SceneManagerExample/Source/SceneManagerExample.h
@@ -1,5 +1,5 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
+#pragma once
 
-#include "SceneManagerExampleGameModeBase.h"
-
+#include "CoreMinimal.h"
