abc4ec99eab5f2539971b976b5a963d5678af579
Quentin Leboutet <quentin.leboutet@tum.de>
2022-08-19 09:55:01 +0200

    removed navigation from AgentController.h

--- code/unreal_plugins/SimulationController/Source/AgentController.h
+++ code/unreal_plugins/SimulationController/Source/AgentController.h
@@ -19,7 +19,4 @@ public:
     virtual std::map<std::string, std::vector<uint8_t>> getObservation() const = 0;
     virtual void reset() = 0;
     virtual bool isReady() const = 0;
-
-    // TODO: 
-    // Navigation navigation_interface;
 };
