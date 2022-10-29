d5d9997a53028cf9e2ed80d27e47c3676b1327c6
Mike Roberts <mike.l.roberts@intel.com>
2022-10-29 12:16:22 -0700

    .

--- code/unreal_plugins/SimulationController/Source/CameraAgentController.cpp
+++ code/unreal_plugins/SimulationController/Source/CameraAgentController.cpp
@@ -292,7 +292,10 @@ void CameraAgentController::buildNavMesh(UNavigationSystemV1* nav_sys)
 
     nav_sys->Build(); // Rebuild NavMesh, required for update AgentRadius
 
-    // We need to wrap this call with guards because ExportNavigationData is only implemented in non-shipping builds
+    // We need to wrap this call with guards because ExportNavigationData(...) is only implemented in non-shipping builds, see:
+    //     Engine/Source/Runtime/Engine/Public/AI/NavDataGenerator.h
+    //     Engine/Source/Runtime/NavigationSystem/Public/NavMesh/RecastNavMeshGenerator.h
+    //     Engine/Source/Runtime/NavigationSystem/Private/NavMesh/RecastNavMeshGenerator.cpp
 #if !(UE_BUILD_SHIPPING || UE_BUILD_TEST)
     if (Config::getValue<bool>({ "SIMULATION_CONTROLLER", "CAMERA_AGENT_CONTROLLER", "NAVMESH", "EXPORT_NAV_DATA_OBJ" })) {
         nav_mesh_->GetGenerator()->ExportNavigationData(FString(Config::getValue<std::string>({ "SIMULATION_CONTROLLER", "CAMERA_AGENT_CONTROLLER", "NAVMESH", "EXPORT_NAV_DATA_OBJ_DIR" }).c_str()) + "/" + world_->GetName() + "/");
