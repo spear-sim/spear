6660db5bac1b9276ebfe04aa9a8d8286d905d151
Mike Roberts <mike.l.roberts@intel.com>
2022-11-11 13:02:44 -0800

    typo corrections

--- code/unreal_plugins/SimulationController/Source/Rpclib.h
+++ code/unreal_plugins/SimulationController/Source/Rpclib.h
@@ -1,13 +1,13 @@
 #pragma once
 
 // Some operating-system–specific header files define nil as nullptr or NULL.
-// RPCLib depends on the msgpack-c library, and msgpack-c has a different
+// rpclib depends on the msgpack-c library, and msgpack-c has a different
 // definition of nil. This creates naming conflicts when we include
 // 'rpc/msgpack.hpp'. However, if MSGPACK_DISABLE_LEGACY_NIL is defined, then
 // msgpack-c does not define nil, and we avoid the conflicts.
 #define MSGPACK_DISABLE_LEGACY_NIL
 
-// RPCLib and Unreal have different definitions for the check macro. This
+// rpclib and Unreal have different definitions for the check macro. This
 // creates naming conflicts when we include 'rpc/msgpack.hpp'. To avoid these
 // conflicts, we undefine the check macro before we include 'rpc/msgpack.hpp',
 // and then redefine check to have the same definition as Unreal defines it
