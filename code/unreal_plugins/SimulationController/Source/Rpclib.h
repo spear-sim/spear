4540b1910010ce88e8608edce96a22a2506f9c55
Mike Roberts <mike.l.roberts@intel.com>
2022-11-11 13:01:44 -0800

    typo corrections

--- code/unreal_plugins/SimulationController/Source/Rpclib.h
+++ code/unreal_plugins/SimulationController/Source/Rpclib.h
@@ -1,13 +1,13 @@
 #pragma once
 
 // Some operating-system–specific header files define nil as nullptr or NULL.
-// RPCLIB depends on the msgpack-c library, and msgpack-c has a different
+// RPCLib depends on the msgpack-c library, and msgpack-c has a different
 // definition of nil. This creates naming conflicts when we include
 // 'rpc/msgpack.hpp'. However, if MSGPACK_DISABLE_LEGACY_NIL is defined, then
 // msgpack-c does not define nil, and we avoid the conflicts.
 #define MSGPACK_DISABLE_LEGACY_NIL
 
-// RPCLIB and Unreal have different definitions for the check macro. This
+// RPCLib and Unreal have different definitions for the check macro. This
 // creates naming conflicts when we include 'rpc/msgpack.hpp'. To avoid these
 // conflicts, we undefine the check macro before we include 'rpc/msgpack.hpp',
 // and then redefine check to have the same definition as Unreal defines it
@@ -40,7 +40,7 @@
 //
 // error C2039: '_InterlockedCompareExchange': is not a member of 'FWindowsPlatformAtomics'
 //
-// To avoid this issue, we need to undefine InterlockedCompareExchange before
+// To avoid this issue, we undefine InterlockedCompareExchange before
 // including any Unreal header files.
 #ifdef InterlockedCompareExchange
 #undef InterlockedCompareExchange
