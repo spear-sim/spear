dbef132e551000dec3af8d662c4dde75d7b25fde
Mike Roberts <mike.l.roberts@intel.com>
2022-11-14 08:22:25 -0800

    typo correction

--- examples/generate_image_dataset/generate_poses.py
+++ examples/generate_image_dataset/generate_poses.py
@@ -4,7 +4,6 @@ import argparse
 import numpy as np
 import os
 import pandas as pd
-import shutil
 import spear
 import sys
 
@@ -41,7 +40,7 @@ if __name__ == "__main__":
 
         # change config based on current scene
         config.defrost()
-        config.SIMULATION_CONTROLLER.WORLD_PATH_NAME = "/Game/Maps/Map_" + scene + "." + "Map_" _ scene
+        config.SIMULATION_CONTROLLER.WORLD_PATH_NAME = "/Game/Maps/Map_" + scene + "." + "Map_" + scene
         config.SIMULATION_CONTROLLER.LEVEL_NAME = "/Game/Maps/Map_" + scene
         config.freeze()
 
