4706ee629aa5a98f9b3345f26967cc935bce8485
MartiFerragut <martiferragut.games@gmail.com>
2022-11-16 15:33:02 +0100

    removing SimpleSim

--- code/unreal_projects/SimpleSim/Source/SimpleSim.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "SimpleSim.h"
-#include <Modules/ModuleManager.h>
-
-IMPLEMENT_PRIMARY_GAME_MODULE(FDefaultGameModuleImpl,
-                                SimpleSim,
-                                "SimpleSim");
