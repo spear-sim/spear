abfc414ea9f31e818692b544708dd9a4065ddc26
MartiFerragut <martiferragut.games@gmail.com>
2022-11-17 09:29:14 +0100

    Resolving compile errors after merge with origin/main

--- code/unreal_plugins/OpenBot/Source/OpenBot.Build.cs
+++ code/unreal_plugins/OpenBot/Source/OpenBot.Build.cs
@@ -6,9 +6,12 @@ public class OpenBot : ModuleRules
     public OpenBot(ReadOnlyTargetRules Target) : base(Target)
     {
         // Disable precompiled headers (in our code but not Unreal code) for faster builds, easier debugging of compile errors, and strict enforcement of include-what-you-use
-        PCHUsage = ModuleRules.PCHUsageMode.Default;
-        PrivatePCHHeaderFile = "";
-        bUseUnity = false;
+        if (Target.Type == TargetType.Editor) {
+            PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+        } else {
+            PCHUsage = ModuleRules.PCHUsageMode.NoPCHs;
+            bUseUnity = false;
+        }
 
         // Turn off code optimization except in shipping builds for faster build times
         OptimizeCode = ModuleRules.CodeOptimization.InShippingBuildsOnly;
