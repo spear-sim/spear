79bbf621db662ea2dee89731928e2ad46a189ecd
Quentin Leboutet <quentin.leboutet@tum.de>
2022-11-29 09:06:40 +0100

    update

--- examples/openbot/imitation_learning/run_trained_policy.py
+++ examples/openbot/imitation_learning/run_trained_policy.py
@@ -49,7 +49,7 @@ if __name__ == "__main__":
     driving_policy = OpenBotPilotNet(config)
     
     # if the user provides a scene_id, use it, otherwise use the scenes defined in scenes.csv
-    if args.scene_id is None:
+    if args.scene_id is [""]:
         scenes_csv_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "scenes.csv")
         assert os.path.exists(scenes_csv_file)
         scene_ids = pd.read_csv(scenes_csv_file, dtype={"scene_id":str})["scene_id"]
