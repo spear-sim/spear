ee85ad64884d1f65d6f20892189bd699bc38de55
rachithp <rachithprakash@gmail.com>
2022-12-01 14:47:31 -0800

    update python setup

--- python/setup.py
+++ python/setup.py
@@ -7,5 +7,5 @@ setuptools.setup(
     author_email="",
     description="",
     packages=["spear"],
-    install_requires=["numpy==1.22.3", "gym==0.21.0", "matplotlib==3.6.2", "opencv-python==4.5.5.64", "pandas==1.5.1", "psutil==5.9.0", "yacs==0.1.8"]
+    install_requires=["numpy==1.22.3", "gym==0.21.0", "h5py==3.7.0", "matplotlib==3.6.2", "opencv-python==4.5.5.64", "pandas==1.5.1", "psutil==5.9.0", "yacs==0.1.8"]
 )
