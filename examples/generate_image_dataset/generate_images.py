5a40c2aaf3fa8d364f7da1bf6d68eebabd13fdbb
MartiFerragut <martiferragut.games@gmail.com>
2022-12-02 19:15:56 +0100

    adding changes

--- examples/generate_image_dataset/generate_images.py
+++ examples/generate_image_dataset/generate_images.py
@@ -85,7 +85,7 @@ if __name__ == "__main__":
 
             # change config based on current scene
             config.defrost()
-            config.SIMULATION_CONTROLLER.WORLD_PATH_NAME = "/Game/Maps/Map_" + scene_id + "." + "Map_" + scene_id + "_defaultLight"
+            config.SIMULATION_CONTROLLER.WORLD_PATH_NAME = "/Game/Maps/Map_" + scene_id + "_defaultLight" + "." + "Map_" + scene_id + "_defaultLight"
             config.SIMULATION_CONTROLLER.LEVEL_NAME = "/Game/Maps/Map_" + scene_id + "_defaultLight"
             config.freeze()
 
