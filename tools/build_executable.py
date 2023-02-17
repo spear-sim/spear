96aa3d52e0e3e554d9b0b5b8a58b8e362da2c21c
RachithP <rachithprakash@gmail.com>
2023-02-16 17:43:28 -0800

    typo corrections

--- tools/build_executable.py
+++ tools/build_executable.py
@@ -34,23 +34,23 @@ if __name__ == "__main__":
         run_uat_script = os.path.join(args.unreal_engine_dir, "Engine", "Build", "BatchFiles", "RunUAT.bat")
         target_platform = "Win64"
         archive_dir = os.path.join(args.output_dir, f"SpearSim-{target_platform}-{args.config_mode}")
-        cmd_prefix = f"conda activate {args.conda_env}&"
+        cmd_prefix = f"conda activate {args.conda_env}& "
     elif sys.platform == "darwin":
         run_uat_script = os.path.join(args.unreal_engine_dir, "Engine", "Build", "BatchFiles", "RunUAT.sh")
         target_platform = "Mac"
         archive_dir = os.path.join(args.output_dir, f"SpearSim-{target_platform}-{args.config_mode}-Unsigned")
         conda_script = args.conda_script
         if not conda_script:
-            conda_script = "~/anaconda3/etc/profile.d/conda.sh"
-        cmd_prefix = f". {conda_script}; conda activate {args.conda_env};"
+            conda_script = os.path.join("~", "anaconda3", "etc", "profile.d", "conda.sh")
+        cmd_prefix = f". {conda_script}; conda activate {args.conda_env}; "
     elif sys.platform == "linux":
         run_uat_script = os.path.join(args.unreal_engine_dir, "Engine", "Build", "BatchFiles", "RunUAT.sh")
         target_platform = "Linux"
         archive_dir = os.path.join(args.output_dir, f"SpearSim-{target_platform}-{args.config_mode}")
         conda_script = args.conda_script
         if not conda_script:
-            conda_script = "~/anaconda3/etc/profile.d/conda.sh"
-        cmd_prefix = f". {conda_script}; conda activate {args.conda_env};"
+            conda_script = os.path.join("~", "anaconda3", "etc", "profile.d", "conda.sh")
+        cmd_prefix = f". {conda_script}; conda activate {args.conda_env}; "
     else:
         assert False
 
@@ -58,31 +58,26 @@ if __name__ == "__main__":
     cmd = ["git", "clone", "--recurse-submodules", "https://github.com/isl-org/spear", repo_dir]
     print(f"[SPEAR | build_executable.py] Executing: {' '.join(cmd)}")
     cmd_result = subprocess.run(cmd, check=True)
-    assert cmd_result.returncode == 0
 
     if args.commit_id:
         cmd = ["git", "reset", "--hard", args.commit_id]
         print(f"[SPEAR | build_executable.py] Executing: {' '.join(cmd)}")
         cmd_result = subprocess.run(cmd, check=True)
-        assert cmd_result.returncode == 0
 
     # build third-party libs
     cmd = ["python", "build_third_party_libs.py", "--num_parallel_jobs", f"{args.num_parallel_jobs}"]
     print(f"[SPEAR | build_executable.py] Executing: {' '.join(cmd)}")
     cmd_result = subprocess.run(cmd, check=True)
-    assert cmd_result.returncode == 0
 
     # create symbolic links
     cmd = cmd_prefix + f"python create_symbolic_links.py --unreal_project_dir {unreal_project_dir} --unreal_plugins_dir {unreal_plugins_dir}"
     print(f"[SPEAR | build_executable.py] Executing: {cmd}")
     cmd_result = subprocess.run(cmd, shell=True, check=True)
-    assert cmd_result.returncode == 0
 
     # generate config file
     cmd = cmd_prefix + f"python generate_config.py --unreal_project_dir {unreal_project_dir}"
     print(f"[SPEAR | build_executable.py] Executing: {cmd}")
     cmd_result = subprocess.run(cmd, shell=True, check=True)
-    assert cmd_result.returncode == 0
     
     # build SpearSim project
     cmd = [
@@ -101,8 +96,7 @@ if __name__ == "__main__":
         "-clientconfig=" + args.config_mode
     ]
     print(f"[SPEAR | build_executable.py] Executing: {' '.join(cmd)}")
-    cmd_result = subprocess.run(cmd)
-    assert cmd_result.returncode == 0
+    cmd_result = subprocess.run(cmd, check=True)
 
     print(f"[SPEAR | build_executable.py] Successfully built SpearSim at {archive_dir}.")
     print(f"[SPEAR | build_executable.py] Done.")
