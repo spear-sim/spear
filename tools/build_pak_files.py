633511844310d01150c3b04cf721422ae6d83351
RachithP <rachithprakash@gmail.com>
2023-03-01 07:01:28 -0800

    address some of the PR comments

--- tools/build_pak_files.py
+++ tools/build_pak_files.py
@@ -5,6 +5,7 @@
 import argparse
 import glob
 import os
+import posixpath
 import subprocess
 import sys
 
@@ -13,88 +14,96 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--unreal_binaries_dir", required=True)
-    parser.add_argument("--unreal_project_dir", required=True)
     parser.add_argument("--scene_id", required=True)
-    parser.add_argument("--platforms", nargs="*", required=True, help="Supported platform values are Windows, Linux, and Mac")
+    parser.add_argument("--platforms", nargs="*", required=True)
     parser.add_argument("--output_dir", required=True)
+    parser.add_argument("--unreal_project_dir")
     args = parser.parse_args()
-
-    os.makedirs(args.output_dir, exist_ok=True)
     
     assert os.path.exists(args.unreal_binaries_dir)
-    unreal_editor_exe = os.path.realpath(os.path.join(args.unreal_binaries_dir, "Win64", "UE4Editor.exe"))
-    unreal_pak_exe    = os.path.realpath(os.path.join(args.unreal_binaries_dir, "Win64", "UnrealPak.exe"))
+    if sys.platform == "win32":
+        unreal_editor_bin = os.path.realpath(os.path.join(args.unreal_binaries_dir, "Win64", "UE4Editor.exe"))
+        unreal_pak_bin    = os.path.realpath(os.path.join(args.unreal_binaries_dir, "Win64", "UnrealPak.exe"))
+    elif sys.platform == "darwin":
+        unreal_editor_bin = os.path.realpath(os.path.join(args.unreal_binaries_dir, "Mac", "UE4Editor"))
+        unreal_pak_bin    = os.path.realpath(os.path.join(args.unreal_binaries_dir, "Mac", "UnrealPak"))
+    elif sys.platform == "linux":
+        unreal_editor_bin = os.path.realpath(os.path.join(args.unreal_binaries_dir, "Linux", "UE4Editor.sh"))
+        unreal_pak_bin    = os.path.realpath(os.path.join(args.unreal_binaries_dir, "Linux", "UnrealPak.sh"))
     
-    assert os.path.exists(args.unreal_project_dir)
-    _, project = os.path.split(args.unreal_project_dir)
-    uproject   = os.path.realpath(os.path.join(args.unreal_project_dir, project + ".uproject"))
+    if args.unreal_project_dir:
+        unreal_project_dirs = [os.path.realpath(args.unreal_project_dir)]
+    else:
+        unreal_projects_dir = os.path.realpath(os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "cpp", "unreal_projects"))
+        unreal_project_dirs = [ os.path.join(unreal_projects_dir, project) for project in os.listdir(unreal_projects_dir) ]
     
-    for platform in args.platforms:
+    for unreal_project_dir in unreal_project_dirs:
 
-        cmd = [ 
-            unreal_editor_exe,
-            uproject,
-            "-run=Cook",
-            "-TargetPlatform=" + platform + "NoEditor",
-            "-fileopenlog",
-            "-ddc=InstalledDerivedDataBackendGraph",
-            "-unversioned",
-            "-stdout",
-            "-FullStdOutLogOutput",
-            "-CrashForUAT",
-            "-unattended",
-            "-NoLogTimes",
-            "-UTF8Output"
-        ]
-        print(f"[SPEAR | build_pak_files.py] Executing: {' '.join(cmd)}")
-        cmd_result = subprocess.run(cmd, stdout=sys.stdout.buffer, stderr=sys.stdout.buffer)
-        assert cmd_result.returncode == 0
+        assert os.path.exists(unreal_project_dir)
+        _, project = os.path.split(unreal_project_dir)
+        uproject   = os.path.realpath(os.path.join(unreal_project_dir, project + ".uproject"))
 
-        content_dirs = [
-            os.path.realpath(os.path.join(args.unreal_project_dir, "Saved", "Cooked", f"{platform}NoEditor", project,  "Content")),
-            os.path.realpath(os.path.join(args.unreal_project_dir, "Saved", "Cooked", f"{platform}NoEditor", "Engine", "Content", "Animation")),
-            os.path.realpath(os.path.join(args.unreal_project_dir, "Saved", "Cooked", f"{platform}NoEditor", "Engine", "Content", "BasicShapes")),
-            os.path.realpath(os.path.join(args.unreal_project_dir, "Saved", "Cooked", f"{platform}NoEditor", "Engine", "Content", "Functions", "Engine_MaterialFunctions02", "Texturing")),
-            os.path.realpath(os.path.join(args.unreal_project_dir, "Saved", "Cooked", f"{platform}NoEditor", "Engine", "Content", "Functions", "Engine_MaterialFunctions02", "Math")),
-            os.path.realpath(os.path.join(args.unreal_project_dir, "Saved", "Cooked", f"{platform}NoEditor", "Engine", "Content", "Functions", "MaterialLayerFunctions")),
-            os.path.realpath(os.path.join(args.unreal_project_dir, "Saved", "Cooked", f"{platform}NoEditor", "Engine", "Content", "Functions", "Engine_MaterialFunctions02", "Utility")),
-            os.path.realpath(os.path.join(args.unreal_project_dir, "Saved", "Cooked", f"{platform}NoEditor", "Engine", "Content", "EngineResources"))
-        ]
+        for platform in args.platforms:
 
-        # construct output pak file path
-        output_pak = os.path.join(args.output_dir, f"Map_{args.scene_id}_{platform}.pak")
+            cmd = [ 
+                unreal_editor_bin,
+                uproject,
+                "-run=Cook",
+                "-TargetPlatform=" + platform + "NoEditor",
+                "-fileopenlog",
+                "-ddc=InstalledDerivedDataBackendGraph",
+                "-unversioned",
+                "-stdout",
+                "-FullStdOutLogOutput",
+                "-CrashForUAT",
+                "-unattended",
+                "-NoLogTimes",
+                "-UTF8Output"
+            ]
+            print(f"[SPEAR | build_pak_files.py] Executing: {' '.join(cmd)}")
+            cmd_result = subprocess.run(cmd, stdout=sys.stdout.buffer, stderr=sys.stdout.buffer, check=True)
 
-        # temporary text file used to generate the final pak file
-        pak_txt_path = os.path.join(args.output_dir, f"pak_data_{platform}.txt")
+            platform_dir = os.path.realpath(os.path.join(unreal_project_dir, "Saved", "Cooked", f"{platform}NoEditor"))
+            content_dirs = [
+                os.path.join(platform_dir, "Engine", "Content", "Animation"),
+                os.path.join(platform_dir, "Engine", "Content", "BasicShapes"),
+                os.path.join(platform_dir, "Engine", "Content", "EngineResources"),
+                os.path.join(platform_dir, "Engine", "Content", "Functions", "Engine_MaterialFunctions02", "Math"),
+                os.path.join(platform_dir, "Engine", "Content", "Functions", "Engine_MaterialFunctions02", "Texturing"),
+                os.path.join(platform_dir, "Engine", "Content", "Functions", "Engine_MaterialFunctions02", "Utility"),
+                os.path.join(platform_dir, "Engine", "Content", "Functions", "MaterialLayerFunctions"),
+                os.path.join(platform_dir, project,  "Content")
+            ]
 
-        if os.path.exists(pak_txt_path):
-            os.remove(pak_txt_path)
+            # create the output_dir
+            os.makedirs(args.output_dir, exist_ok=True)
+            
+            # construct path to the output pak file
+            pak_file = os.path.join(args.output_dir, f"Map_{args.scene_id}_{platform}.pak")
 
-        for dir in content_dirs:
-            with open(pak_txt_path, mode='a') as fout:
-                for fpath in glob.glob(dir + "\**\*.*", recursive=True):
-                    fpath = fpath.replace('\\', "/")
-                    mount_path = fpath.split(f"{platform}NoEditor")[1]
-                    fout.write(f'"{fpath}" "../../..{mount_path}" "" \n')
+            # text file used to generate the final pak file
+            txt_path = os.path.join(args.output_dir, f"Map_{args.scene_id}_{platform}.txt")
 
-        # command to generate the final pak file
-        cmd = [
-            unreal_pak_exe,
-            output_pak,
-            "-create=" + pak_txt_path,
-            "-platform=" + platform,
-            "-multiprocess",
-            "-compress"
-        ]
+            for i, content_dir in enumerate(content_dirs):
+                with open(txt_path, mode="w" if i==0 else "a") as f:
+                    for content_file in glob.glob(os.path.join(content_dir, "**", "*.*"), recursive=True):
+                        content_file = content_file.replace('\\', "/")
+                        mount_file = posixpath.join("..", "..", "..", content_file.split(f"{platform}NoEditor")[1])
+                        f.write(f'"{content_file}" "{mount_file}" "" \n')
 
-        print(f"[SPEAR | build_pak_files.py] Executing: {' '.join(cmd)}")
-        cmd_result = subprocess.run(cmd, stdout=sys.stdout.buffer, stderr=sys.stdout.buffer)
-        assert cmd_result.returncode == 0
+            # command to generate the final pak file
+            cmd = [
+                unreal_pak_bin,
+                pak_file,
+                "-create=" + txt_path,
+                "-platform=" + platform,
+                "-multiprocess",
+                "-compress"
+            ]
+            print(f"[SPEAR | build_pak_files.py] Executing: {' '.join(cmd)}")
+            cmd_result = subprocess.run(cmd, stdout=sys.stdout.buffer, stderr=sys.stdout.buffer, check=True)
 
-        if os.path.exists(output_pak):
-            print(f"[SPEAR | build_pak_files.py] Successfully built {output_pak}.")
-        else:
-            print(f"[SPEAR | build_pak_files.py] Could not build {output_pak} for {platform} platform.")
+            assert os.path.exists(pak_file)
+            print(f"[SPEAR | build_pak_files.py] Successfully built {pak_file} for {platform} platform.")
 
-        # remove the temporary text file
-        os.remove(pak_txt_path)
+    print("[SPEAR | build_pak_files.py] Done.")
