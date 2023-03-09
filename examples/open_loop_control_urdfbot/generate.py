1c090edc5a40359f5c1905211e7fcfefd4213bab
rwang15 <516074749@qq.com>
2023-03-09 15:52:07 +0800

    optimize actions

--- examples/open_loop_control_urdfbot/generate.py
+++ examples/open_loop_control_urdfbot/generate.py
@@ -7,13 +7,15 @@
 import numpy as np
 import pandas as pd
 
+wheel_joints = ["wheel_joint_r", "wheel_joint_l"]
 arm_joints = ["arm_joint_0", "arm_joint_1", "arm_joint_2", "arm_joint_3", "arm_joint_4", "arm_joint_5", "arm_joint_6"]
+gripper_joints = ["gripper_finger_joint_r", "gripper_finger_joint_l"]
 other_joints = ["head_pan_joint", "head_tilt_joint"]
 
 arm_poses = np.pi / 180 * np.array([
     np.array([0, 0, 0, 0, 0., 0, 0.]),
     np.array([-63.87276204, -3.9247609, 89.93145552, 78.6693971, -42.55529432, 80.11668849, 102.91353325]),
-    np.array([67.08168819, 84.27042205, -22.91831181, 95.72957795, 0., 90., 0.]),
+    np.array([-67.08168819, 84.27042205, 22.91831181, 95.72957795, 0., 90., 0.]),
     np.array([-81.94213203, 12.01206017, 107.03768346, 101.74355343, -15.63544527, 75.46713599, 115.29400528])
 ])
 
@@ -22,57 +24,60 @@ def add_action(action_map, joint, name):
     action_map["joint." + joint] = np.array([name], dtype=np.float32)
 
 
-def add_actions(move_forward=0.0, move_right=0.0, gripper_state=50.0, arm_current=0, arm_next=0, arm_aplha=0.0,
+def add_actions(actions_file, move_forward=0.0, move_right=0.0, gripper_state=50.0, arm_current=0, arm_next=0,
+                arm_alpha=0.0,
                 init=False):
     action_map = {}
     # base actions
-    add_action(action_map, "wheel_joint_r", move_forward - move_right)
-    add_action(action_map, "wheel_joint_l", move_forward + move_right)
+    add_action(action_map, wheel_joints[0], move_forward - move_right)
+    add_action(action_map, wheel_joints[1], move_forward + move_right)
 
     # arm actions
     for i in range(len(arm_joints)):
         add_action(action_map, arm_joints[i],
-                   arm_poses[arm_current][i] * (1 - arm_aplha) + arm_poses[arm_next][i] * arm_aplha)
+                   arm_poses[arm_current][i] * (1 - arm_alpha) + arm_poses[arm_next][i] * arm_alpha)
 
-    # gripper actiosn
-    add_action(action_map, "gripper_finger_joint_r", gripper_state)
-    add_action(action_map, "gripper_finger_joint_l", gripper_state)
+    # gripper actions
+    add_action(action_map, gripper_joints[0], gripper_state)
+    add_action(action_map, gripper_joints[1], gripper_state)
 
     # other joints
     for joint in other_joints:
         add_action(action_map, joint, 0.0)
 
+    # save to 
     df = pd.DataFrame(action_map)
-    df.to_csv(episodes_file, mode="w" if init else "a", index=False, header=init)
+    df.to_csv(actions_file, mode="w" if init else "a", index=False, header=init)
 
 
-def fetch_generate_actions(episodes_file):
+def fetch_generate_actions(actions_file):
     # init with headers
-    add_actions(init=True)
+    add_actions(actions_file, init=True)
 
     # move forward
     for i in range(0, 100):
-        add_actions(move_forward=0.01)
+        add_actions(actions_file, move_forward=0.01)
     # hold target
     for i in range(0, 30):
-        add_actions(gripper_state=-50)
+        add_actions(actions_file, gripper_state=-50)
     # rotate base
     for i in range(0, 30):
-        add_actions(move_right=0.009, gripper_state=-100)
+        add_actions(actions_file, move_right=0.009, gripper_state=-100)
     # move forward and move arm
     for i in range(0, 100):
-        add_actions(move_forward=0.01, gripper_state=-100, arm_current=0, arm_next=1, arm_aplha=i / 90)
+        add_actions(actions_file, move_forward=0.01, gripper_state=-100, arm_current=0, arm_next=1, arm_alpha=i / 100)
+
     # release
     for i in range(0, 30):
-        add_actions(gripper_state=50, arm_current=1)
+        add_actions(actions_file, gripper_state=50, arm_current=1)
     for i in range(0, 30):
-        add_actions(move_forward=-0.01, arm_current=1, arm_next=2, arm_aplha=i / 100)
+        add_actions(actions_file, move_forward=-0.01, arm_current=1, arm_next=2, arm_alpha=i / 100)
     for i in range(30, 100):
-        add_actions(move_forward=0, arm_current=1, arm_next=2, arm_aplha=i / 100)
+        add_actions(actions_file, move_forward=0, arm_current=1, arm_next=2, arm_alpha=i / 100)
     for i in range(0, 30):
-        add_actions(arm_current=2)
+        add_actions(actions_file, arm_current=2)
 
 
 if __name__ == '__main__':
-    episodes_file = "actions.csv"
-    fetch_generate_actions(episodes_file)
+    actions_file = "actions.csv"
+    fetch_generate_actions(actions_file)
--- examples/open_loop_control_urdfbot/run.py
+++ examples/open_loop_control_urdfbot/run.py
@@ -5,11 +5,15 @@
 # Before running this file, rename user_config.yaml.example -> user_config.yaml and modify it with appropriate paths for your system.
 
 import argparse
+import shutil
+
 import cv2
 import numpy as np
 import os
 import pandas as pd
 import spear
+
+from examples.open_loop_control_urdfbot.generate import fetch_generate_actions
 
 if __name__ == "__main__":
 
@@ -17,8 +21,14 @@ if __name__ == "__main__":
     parser.add_argument("--benchmark", action="store_true")
     parser.add_argument("--actions_file",
                         default=os.path.join(os.path.dirname(os.path.realpath(__file__)), "actions.csv"))
+    parser.add_argument("--generate_action",
+                        default=True)
     parser.add_argument("--observation_file",
                         default=os.path.join(os.path.dirname(os.path.realpath(__file__)), "observations.csv"))
+    parser.add_argument("--save_camera_obs",
+                        default=True)
+    parser.add_argument("--image_path",
+                        default=os.path.join(os.path.dirname(os.path.realpath(__file__)), "img"))
     args = parser.parse_args()
 
     np.set_printoptions(linewidth=200)
@@ -27,7 +37,15 @@ if __name__ == "__main__":
     config = spear.get_config(
         user_config_files=[os.path.join(os.path.dirname(os.path.realpath(__file__)), "user_config.yaml")])
 
+    # get pregenerated actions for fetch urdf agent
+    if args.generate_action:
+        fetch_generate_actions(args.actions_file)
     df = pd.read_csv(args.actions_file)
+
+    if args.save_camera_obs:
+        if os.path.exists(args.image_path):
+            shutil.rmtree(args.image_path)
+        os.makedirs(args.image_path)
 
     # create Env object
     env = spear.Env(config)
@@ -54,6 +72,8 @@ if __name__ == "__main__":
 
         cv2.imshow("camera.final_color", obs["camera.final_color"])  # note that spear.Env returns BGRA by default
         cv2.waitKey(1)
+        if args.save_camera_obs:
+            cv2.imwrite(f"img/{i:04d}.jpeg",obs["camera.final_color"])
 
         if done:
             env.reset()
