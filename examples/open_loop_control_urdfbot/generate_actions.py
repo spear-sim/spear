47288fe8dcc2d386a2657f5a7fc159d69c04b2a0
rwang15 <516074749@qq.com>
2023-03-16 11:29:07 +0800

    refactor generate_actions

---
+++ examples/open_loop_control_urdfbot/generate_actions.py
@@ -0,0 +1,87 @@
+#
+# Copyright(c) 2022 Intel. Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+#
+
+# generate series of actions for fetch agent on different parts such as moving, arm and gripper.
+
+import argparse
+import os
+import numpy as np
+import pandas as pd
+
+# fetch arm poses come from https://github.com/StanfordVL/iGibson/blob/master/igibson/robots/fetch.py#L100
+arm_poses = {
+    "init": np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]),
+    "default": np.array([-1.17079, 1.47079, 0.4, 1.67079, 0.0, 1.57079, 0.0]),
+    "vertical": np.array([-0.94121, -0.64134, 1.55186, 1.65672, -0.93218, 1.53416, 2.14474]),
+    "diagonal15": np.array([-0.95587, -0.34778, 1.46388, 1.47821, -0.93813, 1.4587, 1.9939]),
+    "diagonal30": np.array([-1.06595, -0.22184, 1.53448, 1.46076, -0.84995, 1.36904, 1.90996]),
+    "diagonal45": np.array([-1.11479, -0.0685, 1.5696, 1.37304, -0.74273, 1.3983, 1.79618]),
+    "horizontal": np.array([-1.43016, 0.20965, 1.86816, 1.77576, -0.27289, 1.31715, 2.01226]),
+}
+
+
+def add_actions(move_forward=0.0, move_right=0.0, gripper_state=50.0, arm_current="init", arm_next="init", arm_alpha=0.0):
+    action_map = {}
+
+    # base joints
+    action_map["joint.wheel_joint_r"] = move_forward - move_right
+    action_map["joint.wheel_joint_l"] = move_forward + move_right
+
+    # arm joints
+    arm_pose = arm_poses[arm_current] * (1.0 - arm_alpha) + arm_poses[arm_next] * arm_alpha
+    action_map["joint.arm_joint_0"] = arm_pose[0]
+    action_map["joint.arm_joint_1"] = arm_pose[1]
+    action_map["joint.arm_joint_2"] = arm_pose[2]
+    action_map["joint.arm_joint_3"] = arm_pose[3]
+    action_map["joint.arm_joint_4"] = arm_pose[4]
+    action_map["joint.arm_joint_5"] = arm_pose[5]
+    action_map["joint.arm_joint_6"] = arm_pose[6]
+
+    # gripper joints
+    action_map["joint.gripper_finger_joint_r"] = gripper_state
+    action_map["joint.gripper_finger_joint_l"] = gripper_state
+
+    # other joints
+    action_map["joint.head_pan_joint"] = 0.0
+    action_map["joint.head_tilt_joint"] = 0.0
+
+    return pd.DataFrame(action_map, index=[0])
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--actions_file", default=os.path.join(os.path.dirname(os.path.realpath(__file__)), "actions.csv"))
+    args = parser.parse_args()
+
+    df = pd.DataFrame()
+
+    # move forward
+    for i in range(0, 100):
+        df = pd.concat([df, add_actions(move_forward=0.01)], ignore_index=True)
+    # hold target
+    for i in range(0, 30):
+        df = pd.concat([df, add_actions(gripper_state=-50)], ignore_index=True)
+    # rotate base
+    for i in range(0, 30):
+        df = pd.concat([df, add_actions(move_right=0.009, gripper_state=-100)], ignore_index=True)
+    # move forward while moving arm
+    for i in range(0, 100):
+        df = pd.concat([df, add_actions(move_forward=0.01, gripper_state=-100, arm_current="init", arm_next="diagonal45", arm_alpha=i / 100)],
+                       ignore_index=True)
+    # release gripper
+    for i in range(0, 30):
+        df = pd.concat([df, add_actions(gripper_state=50, arm_current="diagonal45")], ignore_index=True)
+    # move back and fold arm
+    for i in range(0, 30):
+        df = pd.concat([df, add_actions(move_forward=-0.01, arm_current="diagonal45", arm_next="default", arm_alpha=i / 100)], ignore_index=True)
+    # keep folding arm
+    for i in range(30, 100):
+        df = pd.concat([df, add_actions(move_forward=0, arm_current="diagonal45", arm_next="default", arm_alpha=i / 100)], ignore_index=True)
+    # stay still
+    for i in range(0, 30):
+        df = pd.concat([df, add_actions(arm_current="default")], ignore_index=True)
+
+    # save to csv
+    df.to_csv(args.actions_file, mode="w", index=False, header=True)
--- examples/open_loop_control_urdfbot/run.py
+++ examples/open_loop_control_urdfbot/run.py
@@ -13,22 +13,13 @@ import os
 import pandas as pd
 import spear
 
-from examples.open_loop_control_urdfbot.generate import fetch_generate_actions
-
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--benchmark", action="store_true")
     parser.add_argument("--actions_file",
                         default=os.path.join(os.path.dirname(os.path.realpath(__file__)), "actions.csv"))
-    parser.add_argument("--generate_action",
-                        default=True)
-    parser.add_argument("--observation_file",
-                        default=os.path.join(os.path.dirname(os.path.realpath(__file__)), "observations.csv"))
-    parser.add_argument("--save_camera_obs",
-                        default=True)
-    parser.add_argument("--image_path",
-                        default=os.path.join(os.path.dirname(os.path.realpath(__file__)), "img"))
+    parser.add_argument("--save_images", default=True)
+    parser.add_argument("--image_dir", default=os.path.join(os.path.dirname(os.path.realpath(__file__)), "images"))
     args = parser.parse_args()
 
     np.set_printoptions(linewidth=200)
@@ -38,14 +29,12 @@ if __name__ == "__main__":
         user_config_files=[os.path.join(os.path.dirname(os.path.realpath(__file__)), "user_config.yaml")])
 
     # get pregenerated actions for fetch urdf agent
-    if args.generate_action:
-        fetch_generate_actions(args.actions_file)
     df = pd.read_csv(args.actions_file)
 
-    if args.save_camera_obs:
-        if os.path.exists(args.image_path):
-            shutil.rmtree(args.image_path)
-        os.makedirs(args.image_path)
+    if args.save_images:
+        if os.path.exists(args.image_dir):
+            shutil.rmtree(args.image_dir)
+        os.makedirs(args.image_dir)
 
     # create Env object
     env = spear.Env(config)
@@ -55,26 +44,23 @@ if __name__ == "__main__":
 
     cv2.imshow("camera.final_color", obs["camera.final_color"])  # note that spear.Env returns BGRA by default
     cv2.waitKey(1)
+
     # take a few steps
-    for i in range(df.shape[0]):
-        if config.SIMULATION_CONTROLLER.AGENT == "UrdfBotAgent":
-            action = {}
-            data = df.to_records()[i]
-            for action_key in data.dtype.names:
-                if action_key.startswith("joint"):
-                    action[action_key] = np.array([data[action_key]], dtype=np.float32)
+    if config.SIMULATION_CONTROLLER.AGENT != "UrdfBotAgent":
+        assert False
 
-            obs, reward, done, info = env.step(action=action)
+    for action in df.to_records():
+        for k in action.keys():
+            action[k] = np.array([action[k]], dtype=np.float32)
+        obs, reward, done, info = env.step(action=action)
 
-            print("[SPEAR | run.py] UrdfBotAgent: ", reward, done, info)
-        else:
-            assert False
+        print("[SPEAR | run.py] UrdfBotAgent: ", reward, done, info)
 
         cv2.imshow("camera.final_color", obs["camera.final_color"])  # note that spear.Env returns BGRA by default
         cv2.waitKey(1)
-        if args.save_camera_obs:
-            cv2.imwrite(f"img/{i:04d}.jpeg", obs["camera.final_color"])
 
+        if args.save_camera_obs:
+            cv2.imwrite(os.path.join(args.image_dir, f"{i:04d}.jpeg"), obs["camera.final_color"])
         if done:
             env.reset()
 
