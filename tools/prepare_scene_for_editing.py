e13a649baf703df432cc0e6087a041eea7aa82fa
Mike Roberts <mike.l.roberts@intel.com>
2023-03-27 16:12:04 -0700

    Merge branch 'main' of https://github.com/isl-org/spear into mroberts/prepare-scene

--- tools/prepare_scene_for_editing.py
+++ tools/prepare_scene_for_editing.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--perforce_content_dir", required=True)
     parser.add_argument("--scene_id", required=True)
-    parser.add_argument("--remove_symlink", action="store_true")
+    parser.add_argument("--remove_symlinks", action="store_true")
     args = parser.parse_args()
     
     assert os.path.exists(args.perforce_content_dir)
@@ -31,7 +31,7 @@ if __name__ == '__main__':
         print(f"[SPEAR | prepare_scene_for_editing.py] File or directory or symlink exists, removing: {unreal_project_content_shared_dir}")
         spear.remove_path(unreal_project_content_shared_dir)
 
-    if not args.remove_symlink:
+    if not args.remove_symlinks:
         print(f"[SPEAR | prepare_scene_for_editing.py] Creating symlink: {unreal_project_content_shared_dir} -> {perforce_content_shared_dir}")
         os.symlink(perforce_content_shared_dir, unreal_project_content_shared_dir)
 
@@ -47,7 +47,7 @@ if __name__ == '__main__':
         print(f"[SPEAR | prepare_scene_for_editing.py] File or directory or symlink exists, removing: {unreal_project_content_scene_dir}")
         spear.remove_path(unreal_project_content_scene_dir)
 
-    if not args.remove_symlink:
+    if not args.remove_symlinks:
         print(f"[SPEAR | prepare_scene_for_editing.py] Creating symlink: {unreal_project_content_scene_dir} -> {perforce_content_scene_dir}")
         os.symlink(perforce_content_scene_dir, unreal_project_content_scene_dir)
 
