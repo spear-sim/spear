9079bd444247b8592bc6faebd14ec867fa9ca973
rwang15 <36141951+rwang15@users.noreply.github.com>
2023-03-30 05:55:56 +0800

    Open Loop Control for Fetch in Kujiale Scene (#169)

--- examples/open_loop_control_fetch/generate_actions.py
+++ examples/open_loop_control_fetch/generate_actions.py
@@ -13,21 +13,21 @@ import pandas as pd
 # fetch arm poses come from https://github.com/StanfordVL/iGibson/blob/master/igibson/robots/fetch.py#L100
 arm_poses = {
     "init": np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]),
-    "default": np.array([-1.17079, 1.47079, 0.4, 1.67079, 0.0, 1.57079, 0.0]),
+    "default": np.array([-1.2079, 1.47079, 0.4, 1.67079, 0.0, 1.57079, 0.0]),
     "vertical": np.array([-0.94121, -0.64134, 1.55186, 1.65672, -0.93218, 1.53416, 2.14474]),
     "diagonal15": np.array([-0.95587, -0.34778, 1.46388, 1.47821, -0.93813, 1.4587, 1.9939]),
     "diagonal30": np.array([-1.06595, -0.22184, 1.53448, 1.46076, -0.84995, 1.36904, 1.90996]),
     "diagonal45": np.array([-1.11479, -0.0685, 1.5696, 1.37304, -0.74273, 1.3983, 1.79618]),
     "horizontal": np.array([-1.43016, 0.20965, 1.86816, 1.77576, -0.27289, 1.31715, 2.01226]),
+    "horizontal_high": np.array([-0.94121, -0.30, 1.55186, 1.25672, -0.93218, 0.0, -0.2]),
 }
 
-
-def get_action(move_forward=0.0, move_right=0.0, gripper_force=50.0, arm_pose_blend_weights={"init":1.0}):
+def get_action(move_forward=0.0, move_right=0.0, gripper_force=50.0, arm_pose_blend_weights={"init": 1.0}):
     action = {}
 
     # base joints
-    action["joint.wheel_joint_r"] = move_forward - move_right
-    action["joint.wheel_joint_l"] = move_forward + move_right
+    action["joint.wheel_joint_r"] = move_forward + move_right
+    action["joint.wheel_joint_l"] = move_forward - move_right
 
     # arm joints
     arm_pose = np.zeros([7])
@@ -35,7 +35,7 @@ def get_action(move_forward=0.0, move_right=0.0, gripper_force=50.0, arm_pose_bl
     for pose_name, weight in arm_pose_blend_weights.items():
         arm_pose += arm_poses[pose_name] * weight
         total_weight += weight
-    assert total_weight > 0.0    
+    assert total_weight > 0.0
     arm_pose /= total_weight
     action["joint.arm_joint_0"] = arm_pose[0]
     action["joint.arm_joint_1"] = arm_pose[1]
@@ -55,13 +55,7 @@ def get_action(move_forward=0.0, move_right=0.0, gripper_force=50.0, arm_pose_bl
 
     return action
 
-
-if __name__ == '__main__':
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--actions_file", default=os.path.realpath(os.path.join(os.path.dirname(__file__), "actions.csv")))
-    args = parser.parse_args()
-
+def get_actions_for_starter_content_0000():
     df = pd.DataFrame()
 
     # move forward
@@ -82,7 +76,9 @@ if __name__ == '__main__':
     # move forward while moving arm
     for i in range(0, 100):
         df = pd.concat(
-            [df, pd.DataFrame(get_action(move_forward=0.01, gripper_force=-100.0, arm_pose_blend_weights={"init": (100.0 - i)/100.0, "diagonal45": i/100.0}), index=[0])])
+            [df,
+             pd.DataFrame(get_action(move_forward=0.01, gripper_force=-100.0, arm_pose_blend_weights={"init": (100.0 - i) / 100.0, "diagonal45": i / 100.0}),
+                          index=[0])])
 
     # release gripper
     for i in range(0, 30):
@@ -92,16 +88,94 @@ if __name__ == '__main__':
     # move back and fold arm
     for i in range(0, 30):
         df = pd.concat(
-            [df, pd.DataFrame(get_action(move_forward=-0.01, arm_pose_blend_weights={"diagonal45": (100.0 - i)/100.0, "default": i/100.0}), index=[0])])
+            [df, pd.DataFrame(get_action(move_forward=-0.01, arm_pose_blend_weights={"diagonal45": (100.0 - i) / 100.0, "default": i / 100.0}), index=[0])])
 
     # keep folding arm
     for i in range(30, 100):
         df = pd.concat(
-            [df, pd.DataFrame(get_action(move_forward=0, arm_pose_blend_weights={"diagonal45": (100 - i)/100.0, "default": i/100.0}), index=[0])])
+            [df, pd.DataFrame(get_action(move_forward=0, arm_pose_blend_weights={"diagonal45": (100 - i) / 100.0, "default": i / 100.0}), index=[0])])
 
     # stay still
     for i in range(0, 30):
         df = pd.concat([df, pd.DataFrame(get_action(arm_pose_blend_weights={"default": 1.0}), index=[0])])
 
+    return df
+
+def get_actions_for_kujiale_0000():
+    df = pd.DataFrame()
+
+    # move to target object
+    for i in range(0, 50):
+        df = pd.concat(
+            [df, pd.DataFrame(get_action(move_forward=0.0115, move_right=0.001), index=[0])])
+    for i in range(0, 50):
+        df = pd.concat(
+            [df, pd.DataFrame(get_action(move_forward=0.0115, move_right=-0.001), index=[0])])
+    for i in range(0, 13):
+        df = pd.concat(
+            [df, pd.DataFrame(get_action(move_forward=0.01), index=[0])])
+
+    # hold target
+    for i in range(0, 10):
+        df = pd.concat(
+            [df, pd.DataFrame(get_action(gripper_force=-100.0), index=[0])])
+
+    # move backward
+    for i in range(0, 30):
+        df = pd.concat(
+            [df, pd.DataFrame(get_action(move_forward=-0.01, gripper_force=-100.0), index=[0])])
+
+    # rotate base
+    for i in range(0, 12):
+        df = pd.concat(
+            [df, pd.DataFrame(get_action(move_right=-0.009, gripper_force=-100.0), index=[0])])
+
+    # move and change arm pose
+    for i in range(0, 60):
+        df = pd.concat(
+            [df, pd.DataFrame(
+                get_action(move_forward=0.009, gripper_force=-100.0, arm_pose_blend_weights={"init": (60.0 - i) / 60.0, "horizontal_high": i / 60.0}),
+                index=[0])])
+
+    # move to target pose
+    for i in range(0, 40):
+        df = pd.concat(
+            [df, pd.DataFrame(get_action(move_forward=0.009, gripper_force=-100.0, arm_pose_blend_weights={"horizontal_high": 1}), index=[0])])
+
+    # keep target still
+    for i in range(0, 30):
+        df = pd.concat(
+            [df, pd.DataFrame(get_action(gripper_force=-0, arm_pose_blend_weights={"horizontal_high": 1}), index=[0])])
+
+    # withdraw base
+    for i in range(0, 20):
+        df = pd.concat(
+            [df, pd.DataFrame(get_action(move_forward=-0.009, arm_pose_blend_weights={"horizontal_high": 1}), index=[0])])
+
+    # rotate around and fold arm
+    for i in range(0, 100):
+        df = pd.concat(
+            [df, pd.DataFrame(get_action(move_right=-0.002, arm_pose_blend_weights={"horizontal_high": (100.0 - i) / 100.0, "default": i / 100.0}), index=[0])])
+
+    # stay still
+    for i in range(0, 20):
+        df = pd.concat(
+            [df, pd.DataFrame(get_action(arm_pose_blend_weights={"default": 1}), index=[0])])
+
+    return df
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--scene_id", default=os.path.join(os.path.dirname(os.path.realpath(__file__)), "kujiale_0000"))
+    args = parser.parse_args()
+
+    if args.scene_id == "starter_content_0000":
+        df = get_actions_for_starter_content_0000()
+    elif args.scene_id == "kujiale_0000":
+        df = get_actions_for_kujiale_0000()
+    else:
+        assert False
+
     # save to csv
-    df.to_csv(args.actions_file, float_format="%.5f", mode="w", index=False, header=True)
+    actions_file = os.path.realpath(os.path.join(os.path.dirname(__file__), "actions." + args.scene_id + ".csv"))
+    df.to_csv(actions_file, float_format="%.5f", mode="w", index=False, header=True)
--- examples/open_loop_control_fetch/run.py
+++ examples/open_loop_control_fetch/run.py
@@ -5,7 +5,7 @@
 # Before running this file, rename user_config.yaml.example -> user_config.yaml and modify it with appropriate paths for your system.
 
 import argparse
-import cv2
+import matplotlib.pyplot as plt
 import numpy as np
 import os
 import pandas as pd
@@ -13,13 +13,14 @@ import shutil
 import spear
 import time
 
-
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--actions_file", default=os.path.realpath(os.path.join(os.path.dirname(__file__), "actions.csv")))
-    parser.add_argument("--save_images", default=True)
+    parser.add_argument("--actions_file", default=os.path.realpath(os.path.join(os.path.dirname(__file__), "actions.kujiale_0000.csv")))
     parser.add_argument("--image_dir", default=os.path.realpath(os.path.join(os.path.dirname(__file__), "images")))
+    parser.add_argument("--scene_id", default="kujiale_0000")
+    parser.add_argument("--rendering_mode", default="baked")
+    parser.add_argument("--save_images", action="store_true")
     parser.add_argument("--benchmark", action="store_true")
     args = parser.parse_args()
 
@@ -31,33 +32,72 @@ if __name__ == "__main__":
     # get pregenerated actions for fetch urdf agent
     df = pd.read_csv(args.actions_file)
 
-    if args.save_images:
-        if os.path.exists(args.image_dir):
-            shutil.rmtree(args.image_dir)
-        os.makedirs(args.image_dir)
+    # do some config modifications based on the rendering mode
+    if args.rendering_mode == "baked":
+        rendering_mode_map_str = "_bake"
+        config.defrost()
+        config.SIMULATION_CONTROLLER.CAMERA_SENSOR.FINAL_COLOR_INDIRECT_LIGHTING_INTENSITY = 1.0
+        config.freeze()
+    elif args.rendering_mode == "raytracing":
+        rendering_mode_map_str = "_rtx"
+        config.defrost()
+        config.SIMULATION_CONTROLLER.CAMERA_SENSOR.FINAL_COLOR_INDIRECT_LIGHTING_INTENSITY = 0.0
+        config.freeze()
+    else:
+        assert False
+
+    # change config based on current scene
+    config.defrost()
+
+    if args.scene_id == "starter_content_0000":
+        # starter_content_0000 doesn't need a rendering mode when referring to its map
+        config.SIMULATION_CONTROLLER.SCENE_ID = args.scene_id
+        config.SIMULATION_CONTROLLER.MAP_ID   = args.scene_id
+
+        # starter_content_0000 has scene-specific config values
+        scene_config_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "scene_config.starter_content_0000.yaml")
+
+    elif args.scene_id == "kujiale_0000":
+        config.SIMULATION_CONTROLLER.SCENE_ID = args.scene_id
+        config.SIMULATION_CONTROLLER.MAP_ID   = args.scene_id + rendering_mode_map_str
+
+        # kujiale_0000 has scene-specific config values
+        scene_config_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "scene_config.kujiale_0000.yaml")
+
+    config.merge_from_file(scene_config_file)
+    config.freeze()
 
     # create Env object
     env = spear.Env(config)
 
-    # reset the simulation to get the first observation    
+    # reset the simulation to get the first observation
     obs = env.reset()
 
     if args.benchmark:
         start_time_seconds = time.time()
     else:
-        cv2.imshow("camera.final_color", obs["camera.final_color"])  # note that spear.Env returns BGRA by default
-        cv2.waitKey(1)
+        if args.save_images:
+            if os.path.exists(args.image_dir):
+                shutil.rmtree(args.image_dir)
+            os.makedirs(args.image_dir)
 
-    for i,row in df.iterrows():
-        action = { k:np.array([v], dtype=np.float32) for k,v in row.to_dict().items() }
+    for i, row in df.iterrows():
+        action = {k: np.array([v], dtype=np.float32) for k, v in row.to_dict().items()}
         obs, reward, done, info = env.step(action=action)
 
-        if not args.benchmark:
-            cv2.imshow("camera.final_color", obs["camera.final_color"]) # note that spear.Env returns BGRA by default
-            cv2.waitKey(1)
+        # save images for each render pass
+        if not args.benchmark and args.save_images:
+            for render_pass in config.SIMULATION_CONTROLLER.CAMERA_AGENT.CAMERA.RENDER_PASSES:
+                render_pass_dir = os.path.realpath(os.path.join(args.images_dir, render_pass))
+                assert os.path.exists(render_pass_dir)
 
-        if args.save_images:
-            cv2.imwrite(os.path.realpath(os.path.join(args.image_dir, f"{i:04d}.jpg")), obs["camera.final_color"])
+                obs_render_pass = obs["camera." + render_pass].squeeze()
+                if render_pass in ["final_color", "normals", "segmentation"]:
+                    assert len(obs_render_pass.shape) == 3
+                    assert obs_render_pass.shape[2] == 4
+                    obs_render_pass = obs_render_pass[:, :, [2, 1, 0, 3]].copy()  # note that spear.Env returns BGRA by default
+
+                plt.imsave(os.path.realpath(os.path.join(render_pass_dir, "%04d.png" % i)), obs_render_pass)
 
         if done:
             env.reset()
@@ -66,8 +106,6 @@ if __name__ == "__main__":
         end_time_seconds = time.time()
         elapsed_time_seconds = end_time_seconds - start_time_seconds
         print("[SPEAR | run.py] Average frame time: %0.4f ms (%0.4f fps)" % ((elapsed_time_seconds / df.shape[0])*1000.0, df.shape[0] / elapsed_time_seconds))
-    else:
-        cv2.destroyAllWindows()
 
     # close the environment
     env.close()
