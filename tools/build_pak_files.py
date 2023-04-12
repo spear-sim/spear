6e006abf65666208f807f583816250db8cd08cb9
rachithp <rachithprakash@gmail.com>
2023-04-11 22:53:09 -0700

    update

--- tools/build_pak_files.py
+++ tools/build_pak_files.py
@@ -23,12 +23,15 @@ if __name__ == '__main__':
     parser.add_argument("--scene_ids")
     args = parser.parse_args()
     
+    assert os.path.exists(args.unreal_engine_dir)
+
     unreal_project_dir          = os.path.realpath(os.path.join(os.path.dirname(__file__), "..", "cpp", "unreal_projects", "SpearSim"))
     uproject                    = os.path.realpath(os.path.join(unreal_project_dir, "SpearSim.uproject"))
     unreal_project_content_dir  = os.path.realpath(os.path.join(unreal_project_dir, "Content"))
     output_dir                  = os.path.realpath(args.output_dir)
 
-    assert os.path.exists(args.unreal_engine_dir)
+    # create the output_dir
+    os.makedirs(output_dir, exist_ok=True)
 
     if sys.platform == "win32":
         platform          = "Windows"
@@ -51,7 +54,6 @@ if __name__ == '__main__':
     # once we know the platform, set our cooked dir
     unreal_project_cooked_dir = os.path.realpath(os.path.join(unreal_project_dir, "Saved", "Cooked", platform + "NoEditor"))
 
-    # delete existing contents and create symlinks to perforce content directory
     if args.create_symlinks:
         assert os.path.exists(args.perforce_content_dir)
 
@@ -69,32 +71,17 @@ if __name__ == '__main__':
         print(f"[SPEAR | build_pak_files.py] Creating symlink: {unreal_project_content_shared_dir} -> {perforce_content_shared_dir}")
         os.symlink(perforce_content_shared_dir, unreal_project_content_shared_dir)
 
-    # We do not want to use os.path.realpath(...) here, because that will resolve to the directory inside the user's Perforce workspace.
-    # Instead, we want this path to refer to the symlinked version inside the user's unreal project directory.
-    unreal_project_content_scenes_dir = os.path.join(unreal_project_content_dir, "Scenes")
+        assert os.path.exists(perforce_content_scenes_dir)
+        scene_ids = [ os.path.basename(x) for x in os.listdir(perforce_content_scenes_dir) ]
+        assert len(scene_ids) > 0
 
-    assert os.path.exists(unreal_project_content_scenes_dir)
-    scene_ids = [ os.path.basename(x) for x in os.listdir(unreal_project_content_scenes_dir) ]
-    scene_ids.remove("starter_content_0000")    # no need to build pak file for starter_content_0000 as it is built with the executable
-    assert len(scene_ids) > 0
+        if args.scene_ids is not None:
+            scene_ids = [ s for s in scene_ids if fnmatch.fnmatch(s, args.scene_ids) ]
 
-    if args.scene_ids is not None:
-        scene_ids = [ s for s in scene_ids if fnmatch.fnmatch(s, args.scene_ids) ]
+        assert len(scene_ids) > 0
 
-    assert len(scene_ids) > 0
+        for scene_id in scene_ids:
 
-    for scene_id in scene_ids:
-
-        pak_dirs = [
-            os.path.realpath(os.path.join(unreal_project_cooked_dir, "Engine", "Content")),
-            os.path.realpath(os.path.join(unreal_project_cooked_dir, "SpearSim", "Content", "Shared")),
-            os.path.realpath(os.path.join(unreal_project_cooked_dir, "SpearSim", "Content", "Scenes", scene_id)),
-        ]
-
-        txt_file = os.path.realpath(os.path.join(output_dir, scene_id + "-" + args.version_tag + "-" + platform + ".txt"))
-        pak_file = os.path.realpath(os.path.join(output_dir, scene_id + "-" + args.version_tag + "-" + platform + ".pak"))
-
-        if args.create_symlinks:
             perforce_content_scene_dir = os.path.realpath(os.path.join(perforce_content_scenes_dir, scene_id))
 
             # We do not want to use os.path.realpath(...) here, because that will resolve to the directory inside the user's Perforce workspace.
@@ -108,6 +95,65 @@ if __name__ == '__main__':
             print(f"[SPEAR | build_pak_files.py] Creating symlink: {unreal_project_content_scene_dir} -> {perforce_content_scene_dir}")
             os.symlink(perforce_content_scene_dir, unreal_project_content_scene_dir)
 
+            # see https://docs.unrealengine.com/4.26/en-US/SharingAndReleasing/Deployment/Cooking for more information on these parameters
+            cmd = [
+                unreal_editor_bin,
+                uproject,
+                "-run=Cook",
+                "-targetplatform=" + platform + "NoEditor",
+                "-fileopenlog",
+                "-ddc=InstalledDerivedDataBackendGraph",
+                "-unversioned",
+                "-stdout",
+                "-fullstdoutlogoutput",
+                "-crashforuat",
+                "-unattended",
+                "-nologtimes",
+                "-utf8output"
+            ]
+            print(f"[SPEAR | build_pak_files.py] Executing: {' '.join(cmd)}")
+            subprocess.run(cmd, check=True)
+
+            pak_dirs = [
+                os.path.realpath(os.path.join(unreal_project_cooked_dir, "Engine", "Content")),
+                os.path.realpath(os.path.join(unreal_project_cooked_dir, "SpearSim", "Content", "Shared")),
+                os.path.realpath(os.path.join(unreal_project_cooked_dir, "SpearSim", "Content", "Scenes", scene_id)),
+            ]
+
+            txt_file = os.path.realpath(os.path.join(output_dir, scene_id + "-" + args.version_tag + "-" + platform + ".txt"))
+            pak_file = os.path.realpath(os.path.join(output_dir, scene_id + "-" + args.version_tag + "-" + platform + ".pak"))
+
+            for i, pak_dir in enumerate(pak_dirs):
+                with open(txt_file, mode="w" if i==0 else "a") as f:
+                    for content_file in glob.glob(os.path.realpath(os.path.join(pak_dir, "**", "*.*")), recursive=True):
+                        assert content_file.startswith(unreal_project_cooked_dir)
+                        content_file = content_file.replace('\\', "/")
+                        mount_file = posixpath.join("..", "..", ".." + content_file.split(platform + "NoEditor")[1])
+                        f.write(f'"{content_file}" "{mount_file}" "" \n')
+
+            # construct command to generate the final pak file
+            cmd = [
+                unreal_pak_bin,
+                pak_file,
+                "-create=" + txt_file,
+                "-platform=" + platform,
+                "-multiprocess",
+                "-compress"
+            ]
+            print(f"[SPEAR | build_pak_files.py] Executing: {' '.join(cmd)}")
+            subprocess.run(cmd, check=True)
+
+            assert os.path.exists(pak_file)
+            print(f"[SPEAR | build_pak_files.py] Successfully built {pak_file}")
+
+            print(f"[SPEAR | build_pak_files.py] Removing symlink: {unreal_project_content_scene_dir}")
+            spear.remove_path(unreal_project_content_scene_dir)
+
+        print(f"[SPEAR | build_pak_files.py] Removing symlink: {unreal_project_content_shared_dir}")
+        spear.remove_path(unreal_project_content_shared_dir)
+
+        print("[SPEAR | build_pak_files.py] Done.")
+    else:
         # see https://docs.unrealengine.com/4.26/en-US/SharingAndReleasing/Deployment/Cooking for more information on these parameters
         cmd = [
             unreal_editor_bin,
@@ -126,37 +172,3 @@ if __name__ == '__main__':
         ]
         print(f"[SPEAR | build_pak_files.py] Executing: {' '.join(cmd)}")
         subprocess.run(cmd, check=True)
-
-        # create the output_dir
-        os.makedirs(output_dir, exist_ok=True)
-
-        for i, pak_dir in enumerate(pak_dirs):
-            with open(txt_file, mode="w" if i==0 else "a") as f:
-                for content_file in glob.glob(os.path.realpath(os.path.join(pak_dir, "**", "*.*")), recursive=True):
-                    assert content_file.startswith(unreal_project_cooked_dir)
-                    content_file = content_file.replace('\\', "/")
-                    mount_file = posixpath.join("..", "..", ".." + content_file.split(platform + "NoEditor")[1])
-                    f.write(f'"{content_file}" "{mount_file}" "" \n')
-
-        # construct command to generate the final pak file
-        cmd = [
-            unreal_pak_bin,
-            pak_file,
-            "-create=" + txt_file,
-            "-platform=" + platform,
-            "-multiprocess",
-            "-compress"
-        ]
-        print(f"[SPEAR | build_pak_files.py] Executing: {' '.join(cmd)}")
-        subprocess.run(cmd, check=True)
-
-        assert os.path.exists(pak_file)
-        print(f"[SPEAR | build_pak_files.py] Successfully built {pak_file}")
-
-        print(f"[SPEAR | build_pak_files.py] Removing symlink: {unreal_project_content_scene_dir}")
-        spear.remove_path(unreal_project_content_scene_dir)
-
-    print(f"[SPEAR | build_pak_files.py] Removing symlink: {unreal_project_content_shared_dir}")
-    spear.remove_path(unreal_project_content_shared_dir)
-
-    print("[SPEAR | build_pak_files.py] Done.")
