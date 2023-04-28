e0503b25ac3f3175558da2d91a4d8ebfc0dc38ef
Mike Roberts <mikerobertsphd@gmail.com>
2023-04-28 00:27:39 -0700

    fix UE 5.2 build issue related to symbolic links (#196)
    * fix UE 5.2 build issue related to symbolic links
    
    * safer path remapping code on macOS and Linux
    
    * fixed typo

--- cpp/unreal_projects/SpearSim/Source/SpearSimEditor.Target.cs
+++ cpp/unreal_projects/SpearSim/Source/SpearSimEditor.Target.cs
@@ -10,9 +10,11 @@ public class SpearSimEditorTarget : TargetRules
 {
     public SpearSimEditorTarget(TargetInfo Target) : base(Target)
     {
+        Console.WriteLine("[SPEAR | SpearSimEditor.Target.cs] SpearSimEditorTarget::SpearSimEditorTarget");
+
         Type = TargetType.Editor;
         DefaultBuildSettings = BuildSettingsVersion.V2;
-        ExtraModuleNames.AddRange(new string[] { "SpearSim" });
+        ExtraModuleNames.AddRange(new string[] {"SpearSim"});
 
         if (Target.Platform == UnrealTargetPlatform.Win64) {
 
@@ -26,25 +28,45 @@ public class SpearSimEditorTarget : TargetRules
             bOverrideBuildEnvironment = true;
             AdditionalCompilerArguments = "";
 
-            AdditionalCompilerArguments +=
-                " -ffile-prefix-map=" +
-                Path.Combine(ProjectFile.Directory.FullName, "Plugins") + "=" +
-                Path.GetFullPath(Path.Combine(ProjectFile.Directory.FullName, "..", "..", "unreal_plugins"));
+            string arg = "";
+            Console.WriteLine("[SPEAR | SpearSimEditor.Target.cs] Additional compiler arguments:");
 
-            AdditionalCompilerArguments +=
-                " -ffile-prefix-map=" +
-                Path.Combine(ProjectFile.Directory.FullName, "ThirdParty") + "=" +
-                Path.GetFullPath(Path.Combine(ProjectFile.Directory.FullName, "..", "..", "..", "third_party"));
+            foreach (string pluginDir in Directory.GetDirectories(Path.Combine(ProjectFile.Directory.FullName, "Plugins"))) {
+                string plugin = (new DirectoryInfo(pluginDir)).Name;
 
-            foreach (string plugin in Directory.GetDirectories(Path.Combine(ProjectFile.Directory.FullName, "Plugins"))) {
-                AdditionalCompilerArguments +=
+                // Do the most specific substitution first. If we do a less specific substitution first, then we might not ever perform the
+                // more specific substitution, depending on how the -ffile-prefix-map argument is handled by the compiler.                
+
+                // Old: path/to/spear/cpp/unreal_projects/SpearSim/Plugins/MyPlugin/ThirdParty
+                // New: path/to/spear/third_party
+                arg =
                     " -ffile-prefix-map=" +
-                    Path.Combine(plugin, "ThirdParty") + "=" +
+                    Path.GetFullPath(Path.Combine(pluginDir, "ThirdParty")) + "=" +
                     Path.GetFullPath(Path.Combine(ProjectFile.Directory.FullName, "..", "..", "..", "third_party"));
+                AdditionalCompilerArguments += arg;
+                Console.WriteLine("[SPEAR | SpearSimEditor.Target.cs]     " + arg);
+
+                // Old: path/to/spear/cpp/unreal_projects/SpearSim/Plugins/MyPlugin
+                // New: path/to/spear/cpp/unreal_plugins/MyPlugin
+                arg =
+                    " -ffile-prefix-map=" +
+                    Path.GetFullPath(Path.Combine(pluginDir)) + "=" +
+                    Path.GetFullPath(Path.Combine(ProjectFile.Directory.FullName, "..", "..", "unreal_plugins", plugin));
+                AdditionalCompilerArguments += arg;
+                Console.WriteLine("[SPEAR | SpearSimEditor.Target.cs]     " + arg);
             }
 
+            // Old: path/to/spear/cpp/unreal_projects/SpearSim/ThirdParty
+            // New: path/to/spear/third_party
+            arg =
+                " -ffile-prefix-map=" +
+                Path.GetFullPath(Path.Combine(ProjectFile.Directory.FullName, "ThirdParty")) + "=" +
+                Path.GetFullPath(Path.Combine(ProjectFile.Directory.FullName, "..", "..", "..", "third_party"));
+            AdditionalCompilerArguments += arg;
+            Console.WriteLine("[SPEAR | SpearSimEditor.Target.cs]     " + arg);
+
         } else {
-            throw new Exception("[SPEAR | SpearSimEditor.Target.cs] Target.Platform == " + Target.Platform);            
+            throw new Exception("[SPEAR | SpearSimEditor.Target.cs] Unexpected target platform: " + Target.Platform);            
         }
     }
 }
