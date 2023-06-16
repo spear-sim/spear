f5b51c2ec759196203843befdd00eb6c280d4581
Mike Roberts <mikerobertsphd@gmail.com>
2023-06-16 10:44:59 -0700

    Update SimulationController.Build.cs

--- cpp/unreal_plugins/SimulationController/Source/SimulationController/SimulationController.Build.cs
+++ cpp/unreal_plugins/SimulationController/Source/SimulationController/SimulationController.Build.cs
@@ -10,6 +10,10 @@ public class SimulationController : CommonModuleRules
     {
         SP_LOG_CURRENT_FUNCTION();
 
+        // As a matter of convenience, most Unreal module dependencies can be placed in CommonModuleRules, regardless
+        // of whether or not the module is listed in our uplugin files. These Chaos dependencies are different. If we
+        // place these Chaos module dependencies in CommonModuleRules, then we must also place ChaosVehiclesPlugin in
+        // our uplugin files. So we only list these Chaos module dependencies where they are needed.
         PublicDependencyModuleNames.AddRange(new string[] {"ChaosVehicles", "ChaosVehiclesCore", "CoreUtils", "Vehicle"});
         PrivateDependencyModuleNames.AddRange(new string[] {});
     }
