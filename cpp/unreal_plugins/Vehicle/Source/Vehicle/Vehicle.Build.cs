992888c511df87b2c6027ca61ca8ce765bb32268
Mike Roberts <mikerobertsphd@gmail.com>
2023-06-16 16:39:39 -0700

    Update Vehicle.Build.cs

--- cpp/unreal_plugins/Vehicle/Source/Vehicle/Vehicle.Build.cs
+++ cpp/unreal_plugins/Vehicle/Source/Vehicle/Vehicle.Build.cs
@@ -10,6 +10,12 @@ public class Vehicle : CommonModuleRules
     {
         SP_LOG_CURRENT_FUNCTION();
 
+        // As a matter of convenience, most Unreal module dependencies can be placed in CommonModuleRules, regardless
+        // of whether or not the module is listed in our uplugin files. These Chaos dependencies are different. If we
+        // place these Chaos module dependencies in CommonModuleRules, then we must also place ChaosVehiclesPlugin in
+        // our uplugin files. So we only list these Chaos module dependencies in the Build.cs where they are needed.
+        // ChaosVehiclePlugin is treated differently by the Unreal Engine build system because it is a plugin (i.e.,
+        // it is stored in the Engine/Plugins directory, rather than the Engine/Source directory.
         PublicDependencyModuleNames.AddRange(new string[] {"ChaosVehicles", "ChaosVehiclesCore", "CoreUtils"});
         PrivateDependencyModuleNames.AddRange(new string[] {});
     }
