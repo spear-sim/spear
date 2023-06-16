4e6abced05003896ab6977f3f8e004738c1e2813
RachithP <rachithprakash@gmail.com>
2023-06-16 00:52:25 -0700

    fix Yield macro issue

--- cpp/unreal_plugins/CoreUtils/Source/CoreUtils/Windows.h
+++ cpp/unreal_plugins/CoreUtils/Source/CoreUtils/Windows.h
@@ -7,11 +7,17 @@
 #include <boost/predef.h>
 
 // Unreal and Windows have different definitions for the TEXT macro, so save its state and then restore it.
+// Yield macro is initially undefined, and
+// Windows defines it (https://stackoverflow.com/questions/18909719/for-what-is-the-function-like-macro-yield-in-winbase-h-line-97),
+// so we save the state before and then restore it.
 #if BOOST_OS_WINDOWS
     #pragma push_macro("TEXT")
     #undef TEXT
+    #pragma push_macro("Yield")
+    #undef Yield
 
     #include <Windows/MinWindows.h>
 
+    #pragma pop_macro("Yield")
     #pragma pop_macro("TEXT")
 #endif
