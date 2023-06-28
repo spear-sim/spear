8ae58fcb4671170a1b0b2fd4b77c4c334232cd36
RachithP <rachithprakash@gmail.com>
2023-06-28 00:05:41 -0700

    propagate perforce folder changes to tools

--- tools/prepare_scene_for_editing.py
+++ tools/prepare_scene_for_editing.py
@@ -27,7 +27,7 @@ if __name__ == '__main__':
     perforce_dirs_to_unreal_dirs = {
         os.path.realpath(os.path.join(args.perforce_content_dir, "Megascans")) : os.path.join(unreal_project_content_dir, "Megascans"),
         os.path.realpath(os.path.join(args.perforce_content_dir, "MSPresets")) : os.path.join(unreal_project_content_dir, "MSPresets"),
-        os.path.realpath(os.path.join(args.perforce_content_dir, "Shared")) : os.path.join(unreal_project_content_dir, "Shared"),
+        os.path.realpath(os.path.join(args.perforce_content_dir, "Kujiale")) : os.path.join(unreal_project_content_dir, "Kujiale"),
         os.path.realpath(os.path.join(args.perforce_content_dir, "Scenes", args.scene_id)) : os.path.join(unreal_project_content_dir, "Scenes", args.scene_id)
     }
 
