3fdfc3f9fd868bdd91706940ebd9bda16e066123
rachithp <rachithprakash@gmail.com>
2023-07-20 11:00:26 -0700

    upgrading gym version

--- python/setup.py
+++ python/setup.py
@@ -5,7 +5,7 @@
 import setuptools
 import sys
 
-install_requires_common = ["ffmpeg-python==0.2.0", "gym==0.21.0", "matplotlib==3.6.2", "numpy==1.22.3", "opencv-python==4.5.5.64", "pandas==1.5.1", "psutil==5.9.0", "yacs==0.1.8"]
+install_requires_common = ["ffmpeg-python==0.2.0", "gym==0.22.0", "matplotlib==3.6.2", "numpy==1.22.3", "opencv-python==4.5.5.64", "pandas==1.5.1", "psutil==5.9.0", "yacs==0.1.8"]
 
 if sys.platform == "win32":
     install_requires_platform = ["tensorflow==2.9.3"]
