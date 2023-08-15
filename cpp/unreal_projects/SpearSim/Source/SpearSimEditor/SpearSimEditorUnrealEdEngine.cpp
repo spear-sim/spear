78582e41547ed4a5175ca2343e7678f8fa294332
rachithp <rachithprakash@gmail.com>
2023-08-15 14:08:21 -0700

    half baked code for shitfing dev to windows

--- cpp/unreal_projects/SpearSim/Source/SpearSimEditor/SpearSimEditorUnrealEdEngine.cpp
+++ cpp/unreal_projects/SpearSim/Source/SpearSimEditor/SpearSimEditorUnrealEdEngine.cpp
@@ -5,11 +5,18 @@
 
 #include "SpearSimEditor/SpearSimEditorUnrealEdEngine.h"
 
+#include <map>
 #include <string>
+#include <vector>
 
+
+#include <Components/StaticMeshComponent.h>
 #include <Editor/UnrealEdEngine.h>
+#include <GameFramework/Actor.h>
 
+#include "CoreUtils/Assert.h"
 #include "CoreUtils/Log.h"
+#include "CoreUtils/Std.h"
 #include "CoreUtils/Unreal.h"
 
 USpearSimEditorUnrealEdEngine::USpearSimEditorUnrealEdEngine()
@@ -26,6 +33,23 @@ bool USpearSimEditorUnrealEdEngine::Exec(UWorld* world, const TCHAR* cmd, FOutpu
 {
     std::string cmd_str = Unreal::toStdString(cmd);
     SP_LOG(cmd_str);
+
+    std::vector<std::string> cmd_list = Std::tokenize(cmd_str, " ");
+
+    std::map<std::string, AActor*> all_actors_name_ref_map = Unreal::findActorsByTagAllAsMap(world, {});
+    std::map<AActor*, UStaticMeshComponent*> mesh_actor_ref_map;
+
+    for (auto& element: all_actors_name_ref_map) {
+        
+    }
+
+    if (cmd_list.at(0) == "setObjectLocation") {
+        SP_ASSERT(cmd_list.size() == 5);
+        SP_ASSERT(all_actors_name_ref_map.count(cmd_list.at(1)));
+
+        
+    }
+
 
     return UUnrealEdEngine::Exec(world, cmd, output_device);
 }
