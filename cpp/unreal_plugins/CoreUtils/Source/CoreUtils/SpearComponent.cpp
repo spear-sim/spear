d7897327c986edf440fc233544eea4d96010fe6a
RachithP <rachithprakash@gmail.com>
2023-09-11 15:37:24 -0700

    handle property change callback
    Note that the callback handles only changes to SpearComponent's
    properties. We still need to handle changes to Actor's ActorLabel
    change.

--- cpp/unreal_plugins/CoreUtils/Source/CoreUtils/SpearComponent.cpp
+++ cpp/unreal_plugins/CoreUtils/Source/CoreUtils/SpearComponent.cpp
@@ -5,6 +5,7 @@
 #include "CoreUtils/SpearComponent.h"
 
 #include "CoreUtils/Log.h"
+#include "CoreUtils/Unreal.h"
 
 USpearComponent::USpearComponent(const FObjectInitializer& object_initializer) : UActorComponent(object_initializer)
 {
@@ -15,3 +16,17 @@ USpearComponent::~USpearComponent()
 {
 	SP_LOG_CURRENT_FUNCTION();
 }
+
+#if WITH_EDITOR
+void USpearComponent::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+{
+	SP_LOG_CURRENT_FUNCTION();
+
+	SP_LOG("Property being edited is ", Unreal::toStdString(PropertyChangedEvent.GetMemberPropertyName()));
+	if (PropertyChangedEvent.GetMemberPropertyName() == GET_MEMBER_NAME_CHECKED(USpearComponent, parent_actor_label_name_)) {
+		AActor* owner = this->GetOwner();
+		owner->SetActorLabel(parent_actor_label_name_);
+	}
+	Super::PostEditChangeProperty(PropertyChangedEvent);
+}
+#endif //WITH_EDITOR
