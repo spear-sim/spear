ac10feb24238e5b265c610b7a8142d27339141b0
Mike Roberts <mikerobertsphd@gmail.com>
2024-04-06 02:44:59 -0700

    code cleanup

--- cpp/unreal_plugins/SpCore/Source/SpCore/PropertyArg.h
+++ cpp/unreal_plugins/SpCore/Source/SpCore/PropertyArg.h
@@ -9,7 +9,7 @@
 #include <map>
 #include <string>
 #include <utility>     // std::make_pair
-#include <type_traits> // std::remove_pointer_t
+#include <type_traits> // std::is_pointer_t, std::remove_pointer_t
 #include <vector>
 
 #include "SpCore/Assert.h"
@@ -18,6 +18,26 @@
 #include "SpCore/UnrealClassRegistrar.h"
 
 class UStruct;
+
+class PropertyArgBase;
+
+template <typename TPropertyArgPtr>
+concept CPropertyArgPtr =
+    std::is_pointer_v<TPropertyArgPtr> &&
+    std::derived_from<std::remove_pointer_t<TPropertyArgPtr>, PropertyArgBase>;
+
+template <typename TPropertyArgValueContainer>
+concept CPropertyArgValueContainer =
+    CValueContainer<TPropertyArgValueContainer> &&
+    std::is_pointer_v<typename TPropertyArgValueContainer::value_type> &&
+    std::derived_from<std::remove_pointer_t<typename TPropertyArgValueContainer::value_type>, PropertyArgBase>;
+
+template <typename TPropertyArgKeyValueContainer>
+concept CPropertyArgKeyValueContainer =
+    CKeyValueContainer<TPropertyArgKeyValueContainer> &&
+    std::same_as<typename TPropertyArgKeyValueContainer::key_type, std::string> &&
+    std::is_pointer_v<typename TPropertyArgKeyValueContainer::mapped_type> &&
+    std::derived_from<std::remove_pointer_t<typename TPropertyArgKeyValueContainer::mapped_type>, PropertyArgBase>;
 
 class PropertyArgBase
 {
@@ -57,30 +77,28 @@ public:
     PropertyArgUtils() = delete;
     ~PropertyArgUtils() = delete;
 
-    template <typename TValue> requires
-        std::derived_from<std::remove_pointer_t<TValue>, PropertyArgBase>
-    static void setArgs(std::initializer_list<TValue> property_args, const std::map<std::string, std::string>& property_strings)
+    template <CPropertyArgPtr TPropertyArgPtr>
+    static void setArgs(std::initializer_list<TPropertyArgPtr> property_args, const std::map<std::string, std::string>& property_strings)
     {
-        std::vector<TValue> property_args_vector = Std::toVector<TValue>(property_args);
+        // convert from initializer_list to vector but don't convert the underlying value type
+        std::vector<TPropertyArgPtr> property_args_vector = Std::toVector<TPropertyArgPtr>(property_args);
         setArgs(property_args_vector, property_strings);
     }
 
-    template <CValueContainer TValueContainer> requires
-        std::derived_from<std::remove_pointer_t<typename TValueContainer::value_type>, PropertyArgBase>
-    static void setArgs(const TValueContainer& property_args, const std::map<std::string, std::string>& property_strings)
+    template <CPropertyArgValueContainer TPropertyArgValueContainer>
+    static void setArgs(const TPropertyArgValueContainer& property_args, const std::map<std::string, std::string>& property_strings)
     {
+        // convert PropertyArg<T>* to PropertyArgBase*
         SP_ASSERT(!Std::contains(property_args, nullptr));
-        setArgs(
-            Std::toMap<std::string, PropertyArgBase*>(
-                property_args | std::views::transform([](auto property_arg) { return std::make_pair(property_arg->getName(), property_arg); })),
-            property_strings);
+        auto property_arg_map = Std::toMap<std::string, PropertyArgBase*>(
+            property_args | std::views::transform([](auto property_arg) { return std::make_pair(property_arg->getName(), property_arg); }));
+        setArgs(property_arg_map, property_strings);
     }
 
-    template <CKeyValueContainer TKeyValueContainer> requires
-        std::same_as<typename TKeyValueContainer::key_type, std::string> &&
-        std::derived_from<std::remove_pointer_t<typename TKeyValueContainer::mapped_type>, PropertyArgBase>
-    static void setArgs(const TKeyValueContainer& property_args, const std::map<std::string, std::string>& property_strings)
+    template <CPropertyArgKeyValueContainer TPropertyArgKeyValueContainer>
+    static void setArgs(const TPropertyArgKeyValueContainer& property_args, const std::map<std::string, std::string>& property_strings)
     {
+        // iterate over property strings, set corresponding property according to string
         for (auto& [property_name, property_string] : property_strings) {
             SP_ASSERT(Std::containsKey(property_args, property_name));
             PropertyArgBase* property_arg = property_args.at(property_name);
