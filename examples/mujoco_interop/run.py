707f08ee6a6e59e51678c2df9159539ac4ae993f
rwang15 <516074749@qq.com>
2024-05-28 11:30:57 +0800

    synchronize all static mesh actor

--- examples/mujoco_interop/run.py
+++ examples/mujoco_interop/run.py
@@ -9,15 +9,12 @@ import numpy as np
 import os
 import scipy
 import spear
-
-
-name_prefix = "Meshes/05_chair"
+import scipy.spatial
 
 
 def unreal_rpy_from_mujoco_quaternion(mujoco_quaternion):
-
     # MuJoCo assumes quaternions are stored in scalar-first (wxyz) order, but scipy.spatial.transform.Rotation assumes scalar-last (xyzw) order
-    scipy_quaternion = mujoco_quaternion[[1,2,3,0]]
+    scipy_quaternion = mujoco_quaternion[[1, 2, 3, 0]]
 
     # Unreal and scipy.spatial.transform.Rotation have different Euler angle conventions, see python/spear/pipeline.py for details
     scipy_roll, scipy_pitch, scipy_yaw = scipy.spatial.transform.Rotation.from_quat(scipy_quaternion).as_euler("xyz")
@@ -31,7 +28,7 @@ def unreal_rpy_from_mujoco_quaternion(mujoco_quaternion):
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--mjcf_file", required=True)
+    parser.add_argument("--mjcf_file", default=r"F:\intel\interiorsim\pipeline\apartment_0000\mujoco_scene\main.mjcf")
     args = parser.parse_args()
 
     # create spear Instance
@@ -42,8 +39,9 @@ if __name__ == "__main__":
     # get Unreal actors and functions
     spear_instance.engine_service.begin_tick()
 
-    unreal_actors = spear_instance.unreal_service.find_actors_as_dict()
-    unreal_actors = { unreal_actor_name: unreal_actor for unreal_actor_name, unreal_actor in unreal_actors.items() if unreal_actor_name.startswith(name_prefix) }
+    unreal_actors = spear_instance.unreal_service.find_actors_by_type_as_dict(class_name="AActor")
+    unreal_static_mesh_actors = spear_instance.unreal_service.find_actors_by_type_as_dict(class_name="AStaticMeshActor")
+    unreal_articulated_actors = { unreal_actor_name: unreal_actor for unreal_actor_name, unreal_actor in unreal_actors.items() if unreal_actor_name not in unreal_static_mesh_actors }
 
     unreal_actor_static_class = spear_instance.unreal_service.get_static_class("AActor")
     unreal_set_actor_location_and_rotation_func = spear_instance.unreal_service.find_function_by_name(
@@ -58,13 +56,13 @@ if __name__ == "__main__":
     mujoco.mj_forward(mj_model, mj_data)
 
     # get MuJoCo bodies
-    mj_bodies = { mj_model.body(mj_body).name: mj_body for mj_body in range(mj_model.nbody) if mj_model.body(mj_body).name.startswith(name_prefix) }
+    mj_bodies = {mj_model.body(mj_body).name: mj_body for mj_body in range(mj_model.nbody) if True}
 
     # launch MuJoCo viewer
     mj_viewer = mujoco.viewer.launch_passive(mj_model, mj_data)
 
     # initialize MuJoCo camera (not needed when launching the viewer through the command-line, but needed when using launch_passive)
-    mj_viewer.cam.distance = 30.0*100.0 # 30 meters * 100 Unreal units per meter
+    mj_viewer.cam.distance = 30.0 * 100.0  # 30 meters * 100 Unreal units per meter
     mj_viewer.cam.azimuth = 90.0
     mj_viewer.cam.elevation = -45.0
     mj_viewer.cam.lookat = np.array([0.0, 0.0, 0.0])
@@ -84,14 +82,13 @@ if __name__ == "__main__":
         mj_viewer.sync()
 
         # get updated poses from MuJoCo
-        mj_bodies_xpos = { mj_body_name: mj_data.body(mj_body).xpos for mj_body_name, mj_body in mj_bodies.items() }
-        mj_bodies_xquat = { mj_body_name: mj_data.body(mj_body).xquat for mj_body_name, mj_body in mj_bodies.items() }
+        mj_bodies_xpos = {mj_body_name: mj_data.body(mj_body).xpos for mj_body_name, mj_body in mj_bodies.items()}
+        mj_bodies_xquat = {mj_body_name: mj_data.body(mj_body).xquat for mj_body_name, mj_body in mj_bodies.items()}
 
         # set updated poses in SPEAR
         spear_instance.engine_service.begin_tick()
 
-        for unreal_actor_name, unreal_actor in unreal_actors.items():
-
+        for unreal_actor_name, unreal_actor in unreal_static_mesh_actors.items():
             # call function for each actor
             args = {
                 "NewLocation" : dict(zip(["X", "Y", "Z"], mj_bodies_xpos[unreal_actor_name + ":StaticMeshComponent0"])),
