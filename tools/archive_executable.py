144ab7d5781082965c1a9946d29816000f9c3390
Mike Roberts <mikerobertsphd@gmail.com>
2024-06-15 14:45:12 -0700

    Update archive_executable.py

--- tools/archive_executable.py
+++ tools/archive_executable.py
@@ -13,8 +13,7 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--version_tag", required=True)
-    parser.add_argument("--input_dir", default=os.path.realpath(os.path.join(os.path.dirname(__file__), "BUILD")))
-    parser.add_argument("--output_dir", default=os.path.realpath(os.path.join(os.path.dirname(__file__), "BUILD")))
+    parser.add_argument("--build_dir", default=os.path.realpath(os.path.join(os.path.dirname(__file__), "BUILD")))
     args = parser.parse_args()
 
     if sys.platform == "win32":
@@ -29,8 +28,8 @@ if __name__ == "__main__":
     else:
         assert False
 
-    executable_dir    = os.path.realpath(os.path.join(args.input_dir, f"SpearSim-{platform_name}-Shipping"))
-    archive_file_name = os.path.realpath(os.path.join(args.output_dir, f"SpearSim-{args.version_tag}-{platform_name}-Shipping"))
+    executable_dir    = os.path.realpath(os.path.join(args.build_dir, f"SpearSim-{platform_name}-Shipping"))
+    archive_file_name = os.path.realpath(os.path.join(args.build_dir, f"SpearSim-{args.version_tag}-{platform_name}-Shipping"))
     shutil.make_archive(base_name=archive_file_name, format="zip", root_dir=os.path.realpath(os.path.join(executable_dir, platform_dir_name)), verbose=1)
     assert os.path.exists(archive_file_name + ".zip")
 
