9d7b218726f099a2503c43fab5bd0627106fb409
Mike Roberts <mikerobertsphd@gmail.com>
2024-09-27 13:36:00 -0700

    fix typo

--- python/spear/engine_service.py
+++ python/spear/engine_service.py
@@ -24,8 +24,8 @@ class EngineService():
     # the endian-ness of the Unreal instance. All other services send and receive std::vector<T> where T is
     # not uint8_t, and therefore the endian-ness of the Unreal instance is handled implicitly at the msgpack
     # layer. Since SpFuncService sends and receives std::vector<uint8_t>, the msgpack layer does not attempt
-    # to perform any endian conversions, so sp_func_service.py needs to perform explicit conversions. On the
-    # Unreal instance, we should use Boost to detect endian-ness.
+    # to perform any endian conversions, so sp_func_service.py needs to perform any required conversions
+    # explicitly. On the Unreal instance, we should use Boost to detect endian-ness.
     def get_byte_order(self):
         unreal_instance_byte_order = self._rpc_client.call("engine_service.get_byte_order")
         rpc_client_byte_order = sys.byteorder
