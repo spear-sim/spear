9f636cc3912ddaba5bf9b021357cd0f8e9943cd7
Mike Roberts <mikerobertsphd@gmail.com>
2024-11-05 17:34:45 -0800

    fix typo

--- tools/create_project_symlinks.py
+++ tools/create_project_symlinks.py
@@ -27,48 +27,50 @@ if __name__ == "__main__":
     unreal_plugins_dir = os.path.realpath(args.unreal_plugins_dir)
     third_party_dir    = os.path.realpath(args.third_party_dir)
 
-    # for each plugin dir...
-    for plugin in plugins:
-
-        uplugin = os.path.realpath(os.path.join(unreal_plugins_dir, plugin, plugin + ".uplugin"))
-        assert os.path.exists(uplugin)
-        spear.log(f"Found uplugin: {uplugin}")
-
-        # create a symlink to third_party
-        symlink_third_party_dir = os.path.join(unreal_plugins_dir, plugin, "ThirdParty") # don't want os.path.realpath here
-        if spear.path_exists(symlink_third_party_dir):
-            spear.log(f"    File or directory or symlink exists, removing: {symlink_third_party_dir}")
-            spear.remove_path(symlink_third_party_dir)
-        spear.log(f"    Creating symlink: {symlink_third_party_dir} -> {third_party_dir}")
-        os.symlink(third_party_dir, symlink_third_party_dir)
-
-    # find project
+    # verify that our project is present
     _, project = os.path.split(unreal_project_dir)
     uproject = os.path.realpath(os.path.join(unreal_project_dir, project + ".uproject"))
     assert os.path.exists(uproject)
     spear.log(f"Found uproject: {uproject}")
 
-    # create a symlink to third_party
+    # verify that each of our plugins is present
+    for plugin in plugins:
+        uplugin = os.path.realpath(os.path.join(unreal_plugins_dir, plugin, plugin + ".uplugin"))
+        assert os.path.exists(uplugin)
+        spear.log(f"Found uplugin: {uplugin}")
+
+    # create a ThirdParty symlink in the project dir
     symlink_third_party_dir = os.path.join(unreal_project_dir, "ThirdParty") # don't want os.path.realpath here
     if spear.path_exists(symlink_third_party_dir):
-        spear.log(f"    File or directory or symlink exists, removing: {symlink_third_party_dir}")
+        spear.log(f"File or directory or symlink exists, removing: {symlink_third_party_dir}")
         spear.remove_path(symlink_third_party_dir)
-    spear.log(f"    Creating symlink: {symlink_third_party_dir} -> {third_party_dir}")
+    spear.log(f"Creating symlink: {symlink_third_party_dir} -> {third_party_dir}")
     os.symlink(third_party_dir, symlink_third_party_dir)
 
     # create a Plugins dir in the project dir
     project_plugins_dir = os.path.realpath(os.path.join(unreal_project_dir, "Plugins"))
     os.makedirs(project_plugins_dir, exist_ok=True)
 
-    # create symlink for each plugin listed in the project, if the plugin is in our unreal_plugins dir
+    # for each plugin
     for plugin in plugins:
 
+        spear.log(f"    Creating symlinks for plugin: {plugin}")
+
+        # create a symlink in the Plugins dir
         plugin_dir = os.path.realpath(os.path.join(unreal_plugins_dir, plugin))
         symlink_plugin_dir = os.path.join(project_plugins_dir, plugin) # don't want os.path.realpath here
         if spear.path_exists(symlink_plugin_dir):
-            spear.log(f"    File or directory or symlink exists, removing: {symlink_plugin_dir}")
+            spear.log(f"        File or directory or symlink exists, removing: {symlink_plugin_dir}")
             spear.remove_path(symlink_plugin_dir)
-        spear.log(f"    Creating symlink: {symlink_plugin_dir} -> {plugin_dir}")
+        spear.log(f"        Creating symlink: {symlink_plugin_dir} -> {plugin_dir}")
         os.symlink(plugin_dir, symlink_plugin_dir)
+
+        # create a ThirdParty symlink in the plugin dir
+        symlink_third_party_dir = os.path.join(unreal_plugins_dir, plugin, "ThirdParty") # don't want os.path.realpath here
+        if spear.path_exists(symlink_third_party_dir):
+            spear.log(f"        File or directory or symlink exists, removing: {symlink_third_party_dir}")
+            spear.remove_path(symlink_third_party_dir)
+        spear.log(f"        Creating symlink: {symlink_third_party_dir} -> {third_party_dir}")
+        os.symlink(third_party_dir, symlink_third_party_dir)
 
     spear.log("Done.")
