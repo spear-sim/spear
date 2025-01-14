ddd49c510039634e2134f31051059c2ed6a359a2
Mike Roberts <mikerobertsphd@gmail.com>
2025-01-13 17:47:22 -0800

    improvements to the control_car example

--- examples/control_car/run.py
+++ examples/control_car/run.py
@@ -19,29 +19,18 @@ if __name__ == "__main__":
 
         # find functions
         gameplay_statics_static_class = instance.unreal_service.get_static_class(class_name="UGameplayStatics")
-        get_player_controller_func = instance.unreal_service.find_function_by_name(uclass=gameplay_statics_static_class, function_name="GetPlayerController")
         set_game_paused_func = instance.unreal_service.find_function_by_name(uclass=gameplay_statics_static_class, function_name="SetGamePaused")
-
-        player_controller_static_class = instance.unreal_service.get_static_class(class_name="APlayerController")
-        possess_func = instance.unreal_service.find_function_by_name(uclass=player_controller_static_class, function_name="Possess")
-
-        chaos_vehicle_movement_component_uclass = instance.unreal_service.get_static_class(class_name="UChaosVehicleMovementComponent")
-        set_throttle_input_func = instance.unreal_service.find_function_by_name(uclass=chaos_vehicle_movement_component_uclass, function_name="SetThrottleInput")
 
         # get UGameplayStatics default object
         gameplay_statics_default_object = instance.unreal_service.get_default_object(uclass=gameplay_statics_static_class, create_if_needed=False)
 
-        # get player controller
-        return_values = instance.unreal_service.call_function(uobject=gameplay_statics_default_object, ufunction=get_player_controller_func, args={"PlayerIndex": 0})
-        player_controller = spear.to_handle(return_values["ReturnValue"])
+        # find car
+        car = instance.unreal_service.find_actor_by_type(class_name="AWheeledVehiclePawn")
 
         #
-        # initialize car
-        #
-
-        #
-        # it is also possible to spawn a new car (e.g., the default Unreal off-road car) as follows:
-        #
+        # Instead of finding the existing car in the scene, it is also possible to spawn a new car (e.g., the
+        # default Unreal off-road car) as follows:
+        #        
         # bp_car_uclass = instance.unreal_service.load_object(class_name="UObject", outer=0, name="/Game/VehicleTemplate/Blueprints/OffroadCar/OffroadCar_Pawn.OffroadCar_Pawn_C")
         # car = instance.unreal_service.spawn_actor_from_uclass(
         #     uclass=bp_car_uclass,
@@ -50,32 +39,71 @@ if __name__ == "__main__":
         #     spawn_parameters={"Name": "Agent", "SpawnCollisionHandlingOverride": "AlwaysSpawn"})
         #
 
-        car = instance.unreal_service.find_actor_by_type(class_name="AWheeledVehiclePawn")
-        chaos_vehicle_movement_component = instance.unreal_service.get_component_by_class(actor=car, uclass=chaos_vehicle_movement_component_uclass)
+        #
+        # If we spawned a new car, then we would need to possess it as follows:
+        #
+        # player_controller_static_class = instance.unreal_service.get_static_class(class_name="APlayerController")
+        # get_player_controller_func = instance.unreal_service.find_function_by_name(uclass=gameplay_statics_static_class, function_name="GetPlayerController")
+        # possess_func = instance.unreal_service.find_function_by_name(uclass=player_controller_static_class, function_name="Possess")
+        #
+        # return_values = instance.unreal_service.call_function(uobject=gameplay_statics_default_object, ufunction=get_player_controller_func, args={"PlayerIndex": 0})
+        # player_controller = spear.to_handle(return_values["ReturnValue"])
+        #
+        # instance.unreal_service.call_function(uobject=player_controller, ufunction=possess_func, args={"InPawn": spear.to_ptr(car)})
+        #
 
-        # need player_controller current actor to inject input
-        instance.unreal_service.call_function(uobject=player_controller, ufunction=possess_func, args={"InPawn": spear.to_ptr(car)})
+        #
+        # If we wanted to control the car by calling its UFUNCTIONS directly, rather than by programmatically
+        # injecting gamepad input via Unreal's Enhanced Input System, we would need to obtain handles to the
+        # car's UChaosVehicleMovementComponent and its UFUNCTIONS as follows:
+        #
+        # chaos_vehicle_movement_component_static_class = instance.unreal_service.get_static_class(class_name="UChaosVehicleMovementComponent")
+        # set_steering_input_func = instance.unreal_service.find_function_by_name(uclass=chaos_vehicle_movement_component_static_class, function_name="SetSteeringInput")
+        # set_throttle_input_func = instance.unreal_service.find_function_by_name(uclass=chaos_vehicle_movement_component_static_class, function_name="SetThrottleInput")
+        #
+        # chaos_vehicle_movement_component = instance.unreal_service.get_component_by_class(actor=car, uclass=chaos_vehicle_movement_component_static_class)
+        #
 
     with instance.end_frame():
         pass
 
-    # set throttle to 1.0
+    # set throttle to 1.0 (value will persist across frames until reset)
     with instance.begin_frame():
         instance.unreal_service.call_function(uobject=gameplay_statics_default_object, ufunction=set_game_paused_func, args={"bPaused": False})
-        instance.unreal_service.call_function(uobject=chaos_vehicle_movement_component, ufunction=set_throttle_input_func, args={"Throttle": 1.0})
+
+        #
+        # If we wanted to control the car by calling its UFUNCTIONS directly, we could do so as follows:
+        #
+        # instance.unreal_service.call_function(uobject=chaos_vehicle_movement_component, ufunction=set_throttle_input_func, args={"Throttle": 1.0})
+        #
+
+        instance.enhanced_input_service.inject_input_for_actor(
+            actor=car,
+            input_action_name="IA_Throttle",
+            trigger_event="Triggered",
+            input_action_value={"ValueType": "Axis1D", "Value": {"X": 1.0, "Y": 0.0, "Z": 0.0}},
+            input_action_instance={"TriggerEvent": "Triggered", "LastTriggeredWorldTime": 0.0, "ElapsedProcessedTime": 0.01, "ElapsedTriggeredTime": 0.01})
+
     with instance.end_frame():
         instance.unreal_service.call_function(uobject=gameplay_statics_default_object, ufunction=set_game_paused_func, args={"bPaused": True})
 
-    # drive
+    # drive for multiple frames
     for _ in range(100):
         with instance.begin_frame():
             instance.unreal_service.call_function(uobject=gameplay_statics_default_object, ufunction=set_game_paused_func, args={"bPaused": False})
         with instance.end_frame():
             instance.unreal_service.call_function(uobject=gameplay_statics_default_object, ufunction=set_game_paused_func, args={"bPaused": True})
 
-    # steer
+    # set steering to 1.0 (value will persist across frames until reset)
     with instance.begin_frame():
         instance.unreal_service.call_function(uobject=gameplay_statics_default_object, ufunction=set_game_paused_func, args={"bPaused": False})
+
+        #
+        # If we wanted to control the car by calling its UFUNCTIONS directly, we could do so as follows:
+        #
+        # instance.unreal_service.call_function(uobject=chaos_vehicle_movement_component, ufunction=set_steering_input_func, args={"Steering": 1.0})
+        #
+
         instance.enhanced_input_service.inject_input_for_actor(
             actor=car,
             input_action_name="IA_Steering",
@@ -86,7 +114,7 @@ if __name__ == "__main__":
     with instance.end_frame():
         instance.unreal_service.call_function(uobject=gameplay_statics_default_object, ufunction=set_game_paused_func, args={"bPaused": True})
 
-    # drive
+    # drive for multiple frames
     for _ in range(100):
         with instance.begin_frame():
             instance.unreal_service.call_function(uobject=gameplay_statics_default_object, ufunction=set_game_paused_func, args={"bPaused": False})
@@ -106,10 +134,24 @@ if __name__ == "__main__":
     with instance.end_frame():
         instance.unreal_service.call_function(uobject=gameplay_statics_default_object, ufunction=set_game_paused_func, args={"bPaused": True})
 
-    # set throttle to 1.0 and steer
+    # set throttle to 1.0 and steering to -1.0 (values will persist across frames until reset)
     with instance.begin_frame():
         instance.unreal_service.call_function(uobject=gameplay_statics_default_object, ufunction=set_game_paused_func, args={"bPaused": False})
-        instance.unreal_service.call_function(uobject=chaos_vehicle_movement_component, ufunction=set_throttle_input_func, args={"Throttle": 1.0})
+
+        #
+        # If we wanted to control the car by calling its UFUNCTIONS directly, we could do so as follows:
+        #
+        # instance.unreal_service.call_function(uobject=chaos_vehicle_movement_component, ufunction=set_throttle_input_func, args={"Throttle": 1.0})
+        # instance.unreal_service.call_function(uobject=chaos_vehicle_movement_component, ufunction=set_steering_input_func, args={"Steering": 1.0})
+        #
+
+        instance.enhanced_input_service.inject_input_for_actor(
+            actor=car,
+            input_action_name="IA_Throttle",
+            trigger_event="Triggered",
+            input_action_value={"ValueType": "Axis1D", "Value": {"X": 1.0, "Y": 0.0, "Z": 0.0}},
+            input_action_instance={"TriggerEvent": "Triggered", "LastTriggeredWorldTime": 0.0, "ElapsedProcessedTime": 0.01, "ElapsedTriggeredTime": 0.01})
+
         instance.enhanced_input_service.inject_input_for_actor(
             actor=car,
             input_action_name="IA_Steering",
@@ -120,28 +162,38 @@ if __name__ == "__main__":
     with instance.end_frame():
         instance.unreal_service.call_function(uobject=gameplay_statics_default_object, ufunction=set_game_paused_func, args={"bPaused": True})
 
-    # drive
+    # drive for multiple frames
     for _ in range(100):
         with instance.begin_frame():
             instance.unreal_service.call_function(uobject=gameplay_statics_default_object, ufunction=set_game_paused_func, args={"bPaused": False})
         with instance.end_frame():
             instance.unreal_service.call_function(uobject=gameplay_statics_default_object, ufunction=set_game_paused_func, args={"bPaused": True})
 
-    # set throttle to 0.0
+    # set throttle to 0.0 (value will persist across frames until reset)
     with instance.begin_frame():
         instance.unreal_service.call_function(uobject=gameplay_statics_default_object, ufunction=set_game_paused_func, args={"bPaused": False})
-        instance.unreal_service.call_function(uobject=chaos_vehicle_movement_component, ufunction=set_throttle_input_func, args={"Throttle": 0.0})
+
+        #
+        # If we wanted to control the car by calling its UFUNCTIONS directly, we would do so as follows:
+        #
+        # instance.unreal_service.call_function(uobject=chaos_vehicle_movement_component, ufunction=set_throttle_input_func, args={"Throttle": 0.0})
+        #
+
+        instance.enhanced_input_service.inject_input_for_actor(
+            actor=car,
+            input_action_name="IA_Throttle",
+            trigger_event="Triggered",
+            input_action_value={"ValueType": "Axis1D", "Value": {"X": 0.0, "Y": 0.0, "Z": 0.0}},
+            input_action_instance={"TriggerEvent": "Triggered", "LastTriggeredWorldTime": 0.0, "ElapsedProcessedTime": 0.01, "ElapsedTriggeredTime": 0.01})
+
     with instance.end_frame():
         instance.unreal_service.call_function(uobject=gameplay_statics_default_object, ufunction=set_game_paused_func, args={"bPaused": True})
 
-    # coast
+    # drive for multiple frames
     for _ in range(100):
         with instance.begin_frame():
             instance.unreal_service.call_function(uobject=gameplay_statics_default_object, ufunction=set_game_paused_func, args={"bPaused": False})
         with instance.end_frame():
             instance.unreal_service.call_function(uobject=gameplay_statics_default_object, ufunction=set_game_paused_func, args={"bPaused": True})
-
-    # close the instance
-    instance.close()
 
     spear.log("Done.")
