bcc87930cdb98b28373d790951f0d8a015084bc4
Mike Roberts <mikerobertsphd@gmail.com>
2025-10-31 10:30:11 -0700

    added dummy blueprint function library example

---
+++ cpp/unreal_plugins/SpUnrealTypes/Source/SpUnrealTypes/SpDummyBlueprintFunctionLibrary.h
@@ -0,0 +1,34 @@
+//
+// Copyright(c) 2025 The SPEAR Development Team. Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+// Copyright(c) 2022 Intel. Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+//
+
+#pragma once
+
+#include <Containers/UnrealString.h> // FString
+#include <Kismet/BlueprintFunctionLibrary.h>
+
+#include "SpDummyBlueprintFunctionLibrary.generated.h"
+
+//
+// The purpose of USpDummyBlueprintFunctionLibrary is to provide a minimal blueprint function library with
+// all of the boilerplate required to implement a simple custom function that is accessible from Python. If
+// you want to create a new blueprint function library for use with the SPEAR Python API, USpDummyBlueprintFunctionLibrary
+// is a good starting point.
+//
+
+UCLASS()
+class USpDummyBlueprintFunctionLibrary : public UBlueprintFunctionLibrary
+{
+    GENERATED_BODY()
+public: 
+
+    // The BlueprintCallable decoration is not required for this function to be visible to the SPEAR Python
+    // API, but it will make it so the function is visible to Unreal's built-in Python API and in Unreal's
+    // blueprint editor.
+    UFUNCTION(BlueprintCallable, Category="SPEAR")
+    static FString HelloWorld()
+    {
+        return "Hello World";
+    }
+};
