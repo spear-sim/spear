329475271e13eedd449b4beb8eece5cb105c80f1
Mike Roberts <mikerobertsphd@gmail.com>
2026-07-07 22:32:37 -0700

    Update system_utils.py

--- python/spear/utils/system_utils.py
+++ python/spear/utils/system_utils.py
@@ -4,8 +4,7 @@
 #
 
 import os
-
-from .log_utils import log
+import spear
 
 # perform configuration steps that should only be done once per system, as opposed to once per instance
 def configure_system(config):
@@ -13,5 +12,5 @@ def configure_system(config):
     # set environment variables
     if config.SPEAR.LAUNCH_MODE in ["editor", "game"]:
         for environment_var_name, environment_var_value in config.SPEAR.ENVIRONMENT_VARS.items():
-            log("Setting environment variable: ", environment_var_name, " = ", environment_var_value)
+            spear.log("Setting environment variable: ", environment_var_name, " = ", environment_var_value)
             os.environ[environment_var_name] = environment_var_value
