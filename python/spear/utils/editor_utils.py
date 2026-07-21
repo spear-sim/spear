c9d301d6cc10ba43d0af0880fa53825ff1118550
Jakub Tomsu <jtomsu@adobe.com>
2026-07-20 17:01:31 -0700

    fix issues found by linter

--- python/spear/utils/editor_utils.py
+++ python/spear/utils/editor_utils.py
@@ -383,7 +383,7 @@ def to_unreal_transform_from_numpy_transform(numpy_transform):
     return to_unreal_transform_from_spear_transform(spear_transform=spear.math.to_spear_transform_from_numpy_transform(numpy_transform=numpy_transform))
 
 def to_unreal_transform_from_numpy_matrix(matrix):
-    return to_unreal_transform_from_spear_transform(spear_transform=spear.math.to_spear_transform_from_numpy_matrix(numpy_matrix=numpy_matrix))
+    return to_unreal_transform_from_spear_transform(spear_transform=spear.math.to_spear_transform_from_numpy_matrix(numpy_matrix=matrix))
 
 # unreal.Vector
 
